81413b070a633f97e4140039d06620ac3250a7ba
franck <franck@e5f3cfe4-adf0-4648-8384-9a324bcb4d69>
2010-09-15 06:41:34 +0000

    a better way of completely rebuilding the code
    git-svn-id: https://franck.privatedns.org/svn/public_notebook@20 e5f3cfe4-adf0-4648-8384-9a324bcb4d69

--- pythonout.sh
+++ pythonout.sh
@@ -9,12 +9,12 @@ cat $1 | sed "s/\/mnt\/windows\/linuxfiles/\/home\/john\/data/" > temp.py
 mv temp.py $1
 #rm temp.py
 if [ -e "$1.old" ] && [ -e "$1" ]; then
-#	if ! diff $1.old $1; then
-    echo "running diff regardless of update status"
-    # if the old version and the new version are different, then run python, otherwise, do nothing
-    python -W "ignore" $1 > $1.out 2> $1.err
-    mv $1 $1.old
-#	fi
+	if ! diff $1.old $1; then
+        echo "running diff regardless of update status"
+        # if the old version and the new version are different, then run python, otherwise, do nothing
+        python -W "ignore" $1 > $1.out 2> $1.err
+        mv $1 $1.old
+	fi
 else
 	# also need to run if both files don't exist
 	echo "$1 either old or new doesn't exist... creating"
