85a2e644ff82368281926aa13842b26bfddb6070
franck <franck@e5f3cfe4-adf0-4648-8384-9a324bcb4d69>
2010-11-15 20:17:19 +0000

    update
    git-svn-id: https://franck.privatedns.org/svn/public_notebook@27 e5f3cfe4-adf0-4648-8384-9a324bcb4d69

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 rm $1.*.py.old $1.*.py.err
-pdflatex --shell-escape $1
-pdflatex --shell-escape $1
+#pdflatex --shell-escape $1
+#pdflatex --shell-escape $1
+make FILE=$1 NOBIB=1 BASEFILE=article_wrapper.tex plain
