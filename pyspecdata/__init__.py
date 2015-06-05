302aa965cb92b4cbfa08fd591809f2434ce89ef9
John Franck <jmf356@cornell.edu>
2015-06-05 17:00:39 -0400

    modified to fix the import * problem and added the lapack binaries

--- pyspecdata/__init__.py
+++ pyspecdata/__init__.py
@@ -1,3 +1,12 @@
 from .matlablike import *
+#import numpy
 
-__all__ = ['prop']
+# so essentially, __all__ is the namespace that is passed with an import *
+#__all__ = ['prop',
+#        'nddata',
+#        'figlist_var',
+#        'plot',
+#        'OLDplot',
+#        'nddata_hdf5']
+#__all__.extend(numpy.__all__)
+__all__ = filter(lambda x: x[0] != '_',dir())
--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #from setuptools import setup
 import setuptools # I think this is needed for the following
 from numpy.distutils.core import Extension,setup
+import os
 #ext_prop = Extension(name = 'propagator',
 #        sources = ['propagator.f90'],
 #        f2py_options = '--fcompiler=gnu95 -llapack',
@@ -10,9 +11,19 @@ ext_test = Extension(name = 'pyspecdata.test_module',
         define_macros = [('ADD_UNDERSCORE',None)],
         )
 #f2py_options = ['--fcompiler=gnu95','--compiler=mingw32','-lmsvcr71'])
-ext_prop = Extension(name = 'pyspecdata.propagator',
-        sources = ['pyspecdata/propagator.f90'],
-        libraries = ['lapack','refblas'])
+
+kwargs = {}
+if os.name == 'nt':
+    print "It looks like you're on windows, so I'm going to try to use a version of lapack (from http://netlib.org/) that was built with MinGW.  If this fails, you should get MinGW"
+    kwargs.update(dict(library_dirs = ['.']))
+
+try:
+    ext_prop = Extension(name = 'pyspecdata.propagator',
+            sources = ['pyspecdata/propagator.f90'],
+            libraries = ['lapack','refblas'],
+            **kwargs)
+except:
+    raise RuntimeError("There was as an error -- probabably becuase of lapack\nIf you want to no more, remove the try/except clause in setup.py")
 
 setup(
     name='pySpecData',
@@ -28,6 +39,11 @@ setup(
         "scipy",
         "matplotlib",
         "tables",
+        "mayavi",
+        "PyQt4",
         ],
     ext_modules = [ext_test,ext_prop],
+#    entry_points=dict(
+#        notebook_info=["data_dir = pyspecdata:datadir ["+os.path.expanduser('~')+os.path.sep+'exp_data]']
+#        )
 )
