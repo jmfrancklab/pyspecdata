b33cb3d59c4158258293e34eb7a0f78b0f553051
John Franck <jmf356@cornell.edu>
2015-11-22 22:50:25 -0500

    fixed an issue with the skew function

--- pyspecdata/fourier/skew.py
+++ pyspecdata/fourier/skew.py
@@ -2,12 +2,12 @@ from ..general_functions import *
 from pylab import * 
 
 def skew(self,along_axis,by_amount,propto_axis,zero_fill = True):
-    kwargs = {}
+    kwargs = {'pad':False}
     if zero_fill:
         n = self.data.shape[self.axn(along_axis)]
         n = int(2**(ceil(log2(n))))
         n *= 2
-        {'pad':n}
+        kwargs['pad'] = n
     self.ft(along_axis,shift = None,**kwargs)
     newdata = self * self.fromaxis([along_axis,propto_axis],
             lambda x,y: exp(-2j*pi*by_amount*x*y))
