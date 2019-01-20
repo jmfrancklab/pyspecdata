a4931cb62ee6753dd1a10040e59ab1238cdfe5ff
John Franck (laptop) <jmfranck@syr.edu>
2019-01-20 15:53:50 -0500

    now, I have a draft of the nnls example

--- pyspecdata/nnls.py
+++ pyspecdata/nnls.py
@@ -11,7 +11,7 @@ from multiprocessing import cpu_count
 __all__ = ['nnls_regularized']
 
 
-def nnls(self, dimname, newaxis_dict, kernel_func, l):
+def nnls(self, dimname, newaxis_dict, kernel_func, l=0):
     r"""Perform regularized non-negative least-squares "fit" on self.
 
     .. todo::
@@ -20,17 +20,24 @@ def nnls(self, dimname, newaxis_dict, kernel_func, l):
     Parameters
     ==========
     dimname: str
-        Name of the "old" dimension that is to be replaced by a distribution.
+        Name of the "data" dimension that is to be replaced by a
+        distribution (the "fit" dimension);
         *e.g.* if you are regularizing a set of functions
         :math:`\exp(-\tau*R_1)`, then this is :math:`\tau`
     newaxis_dict: dict
-        a dictionary whose key is the name of the new dimension
+        a dictionary whose key is the name of the "fit" dimension
         (:math:`R_1` in the example above)
         and whose value is an array with the new axis labels
     kernel_func: function
-        a function giving the kernel for the regularization.  The first argument is the "old" variable
-        and the second argument is the "new" value
-        (in the example above, this would be something like ``lambda x,y: exp(-x*y)``)
+        a function giving the kernel for the regularization.
+        The first argument is the "data" variable
+        and the second argument is the "fit" variable
+        (in the example above, this would be something like
+        ``lambda x,y: exp(-x*y)``)
+    l : double (default 0)
+        the regularization parameter
+        :math:`lambda` -- if this is set to 0, the algorithm reverts to
+        standard nnls
 
     Returns
     =======
@@ -39,12 +46,45 @@ def nnls(self, dimname, newaxis_dict, kernel_func, l):
         For future use, both the kernel (as an nddata, in a property called
         "nnls_kernel") and the residual (as an nddata, in a property called
         "nnls_residual") are stored as properties of the nddata.
+        The regularized dimension is always last
+        (innermost).
     """
     assert type(dimname) is str, "first argument is dimension name"
     assert type(newaxis_dict) is dict, "second argument is dictionary with new axis"
+    assert len(newaxis_dict) == 1, "currently only set up for 1D"
     assert callable(kernel_func), "third argument is kernel function"
-    # construct the 
+    # construct the kernel
+    # the kernel transforms from (columns) the "fit" dimension to (rows)
+    # the "data" dimension
+    fitdim_name = newaxis_dict.keys().item()
+    fit_axis = nddata(newaxis_dict[fitdim_name], fitdim_name)
+    data_axis = self.fromaxis(dimname)
+    K = fit_axis*data_axis
+    self.reorder(dimname, first=False) # make the dimension we will be regularizing innermost
+    data_fornnls = self.data
+    if len(data_fornnls) > 2:
+        data_fornnls = data_fornnls.reshape(prod(
+            data_fornnls.shape[:-1]),data_fornnls.shape[-1])
+    retval, residual = nnls_regularized(K, data_fornnls, l=l)
+    newshape = []
+    if not isscalar(l):
+        newshape.append(len(l))
+        self.dimlabels = ['lambda'] + self.dimlabels
+        self.setaxis('lambda',l)
+    newshape += list(self.data.shape)
+    newshape.append(len(newaxis_dict[fitdim_name]))
+    retval = retval.reshape(newshape)
+    self.data = retval
+    # change the dimension names and data
+    self.rename(dimname, fitdim_name)
+    self.setaxis(fitdim_name, newaxis_dict[fitdim_name])
+    self.data = retval
+    # make the residual nddata as well
+    residual_nddata = ndshape(self).pop(dimname).alloc()
+    residual_nddata.data[:] = residual[:]
     # store the kernel and the residual in the properties
+    self.set_prop('nnls_kernel',K)
+    self.set_prop('nnls_residual',residual_nddata)
     return self
 def nnls_regularized(A, b, l=0, maxiter=None):
     """
@@ -128,14 +168,14 @@ def nnls_regularized(A, b, l=0, maxiter=None):
                     w = zeros((n,), dtype=double)
                     zz = zeros((m+n,), dtype=double)
                     index = zeros((n,), dtype=int)
-                    return _nnls.nnls_regularized(A, b, w, zz, index, maxiter, l)
+                    x, rnorm, mode = _nnls.nnls_regularized(A, b, w, zz, index, maxiter, l)
             if len(b.shape) == 2:
                 def nnls_func(l):
                     w = zeros((n,), dtype=double)
                     zz = zeros((m+n,), dtype=double)
                     index = zeros((n,), dtype=int)
                     x, rnorm, mode = _nnls.nnls_regularized_loop(A, redim_C_to_F(b), w, zz, index, maxiter, l)
-                    return redim_F_to_C(x), rnorm, mode
+                    x = redim_F_to_C(x)
             retval = p.map(nnls_func,l)
             x,rnorm,mode = map(np_array,zip(*retval))
     if (isscalar(mode) and mode != 1):
