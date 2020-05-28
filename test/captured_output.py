cfd0d3e6fb1d718b9daa78085ed05f2e47c4a20b
John Franck (laptop) <jmfranck@syr.edu>
2020-05-28 11:13:49 -0400

    turned logging example into a unittest
    this is actually a super complicated example of a unittest,
    since I'm capturing the standard output
    (other unittests should be much simpler)

---
+++ test/captured_output.py
@@ -0,0 +1,15 @@
+# stolen wholesale from here:
+# https://stackoverflow.com/questions/4219717/how-to-assert-output-with-nosetest-unittest-in-python
+import sys
+from contextlib import contextmanager
+from io import StringIO
+
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
