a6b1e3e13f6eb7e146b93e1e62235db2008ba3d6
John Franck (laptop) <jmfranck@syr.edu>
2020-07-07 11:07:01 -0400

    finished making changes in previous

--- pyspecdata/core.py
+++ pyspecdata/core.py
@@ -4421,7 +4421,7 @@ class nddata (object):
             except TypeError:
                 self.data = func(self.getaxis(axis),self.data,axes=thisaxis)
         else:
-            if numnonoptargs == 1 or len(getargspec(func).varargs)>0:
+            if numnonoptargs == 1 or numvarargs>0:
                 try:
                     self.data = func(self.data,axis=thisaxis)
                 except TypeError:
