89863f53c4c9e1fc34a92776fac71c5d306c26be
Alec Beaton (laptop) <aabeaton@syr.edu>
2020-09-04 17:10:28 -0400

    removing items from debug

--- pyspecdata/core.py
+++ pyspecdata/core.py
@@ -5459,13 +5459,6 @@ class nddata (object):
         start_index = 0
         stop_index = len(u)
         stop_index = extent 
-        print("*** *** *** *** ***")
-        print(extent)
-        print(u[-1])
-        print(du)
-        print(extent*du-u[-1])
-        print(int((extent - u[-1]) // du))
-        print("*** *** *** *** ***")
         if extent < u[0]:
             start_index = int(-(u[0] - extent) // du) # the part after the negative is positive
             if (start_index * du + (u[0] - extent))/du < -tolerance:# the first quantity here is negative
