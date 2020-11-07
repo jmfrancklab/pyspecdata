6c0364a18d98729064a6ba8508e6d1ba50ad8d1a
John Franck (laptop) <jmfranck@syr.edu>
2020-11-07 11:45:36 -0500

    fixed a bunch of stuff alex messed up

--- pyspecdata/core.py
+++ pyspecdata/core.py
@@ -5393,7 +5393,7 @@ class nddata (object):
                 else:
                     axis_data = self.getaxis(axisname).flatten()
                     # copy is needed here, or data and axis will be the same object
-                    retval = nddata(axis_data,axis_data.shape,[axisname]).setaxis(axisname,copy(axis_data))
+                    retval = nddata(axis_data,axis_data.shape,[axisname]).setaxis(axisname,np.copy(axis_data))
                     if self.axis_coords_units is None:
                         retval.axis_coords_units = None
                     else:
