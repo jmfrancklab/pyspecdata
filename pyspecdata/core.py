b3a2b53e31d6b6964d087ccaafb750b9b9b6ff93
John Franck (laptop) <jmfranck@syr.edu>
2021-01-08 17:17:11 -0500

    ticks weren't working well again, so fix

--- pyspecdata/core.py
+++ pyspecdata/core.py
@@ -106,11 +106,8 @@ rcParams['legend.fontsize'] = 12
 rcParams['axes.grid'] = False
 rcParams['font.size'] = 18
 rcParams['image.cmap'] = 'jet'
-rcParams['figure.figsize']=(7*(1+sqrt(5))/2,7)
-if inside_sphinx():
-    mat2array = []
-else:
-    mat2array = [{'ImmutableMatrix': array}, 'numpy']# for sympy returns arrays rather than the stupid matrix class
+rcParams['figure.figsize']=(7*(1+np.sqrt(5))/2,7)
+mat2array = [{'ImmutableMatrix': np.array}, 'numpy']# for sympy returns arrays rather than the stupid matrix class
 logger = logging.getLogger('pyspecdata.core')
 #{{{ constants
 k_B = 1.380648813e-23
@@ -1256,6 +1253,8 @@ def gridandtick(ax,rotation=(0,0),precision=(2,2),
         formatonly = False,fixed_y_locator = None,
         use_grid = True,
         spines = None,y = True):
+    defaultMajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
+    defaultMinorLocator = lambda: mticker.AutoMinorLocator(n=5)
     #{{{ taken from matplotlib examples
     def adjust_spines(ax,spines):
         xlabel = ax.get_xlabel()
@@ -1291,10 +1290,10 @@ def gridandtick(ax,rotation=(0,0),precision=(2,2),
         widthexp = floor(log(width)/log(10.))-1
         scalefactor = 10**widthexp
         width /= scalefactor
-        majorLocator = MaxNLocator(nbins='auto', steps=[1,5,10])
+        majorLocator = defaultMajorLocator()
         #majorFormatter = FormatStrFormatter('%0.'+'%d'%precision[0]+'f'+labelstring[0])# labelstring can be used, for instance, for pi
         #ax.xaxis.set_major_formatter(majorFormatter)
-        minorLocator = MaxNLocator(min_n_ticks=5, nbins=5,steps=[1,5,10])
+        minorLocator = defaultMinorLocator()
         ax.xaxis.set_major_locator(majorLocator)
         #for the minor ticks, use no labels; default NullFormatter
         ax.xaxis.set_minor_locator(minorLocator)
@@ -1314,8 +1313,8 @@ def gridandtick(ax,rotation=(0,0),precision=(2,2),
                     majorLocator = LogLocator(10)
                     minorLocator = LogLocator(10,subs=r_[0:11])
                 else:
-                    majorLocator = MaxNLocator(nbins='auto', steps=[1,5,10])
-                    minorLocator = MaxNLocator(nbins=5,steps=[1,5,10])
+                    majorLocator = defaultMajorLocator()
+                    minorLocator = defaultMinorLocator()
             else:
                 majorLocator = MultipleLocator(fixed_y_locator[4::5])
                 minorLocator = FixedLocator(fixed_y_locator)
@@ -1325,10 +1324,11 @@ def gridandtick(ax,rotation=(0,0),precision=(2,2),
             #for the minor ticks, use no labels; default NullFormatter
             ax.yaxis.set_minor_locator(minorLocator)
             #}}}
-    ax.yaxis.grid(use_grid,which='major',color=gridcolor,alpha=0.15,linestyle='-')
-    ax.xaxis.grid(use_grid,which='major',color=gridcolor,alpha=0.15,linestyle='-')
-    ax.yaxis.grid(use_grid,which='minor',color=gridcolor,alpha=0.075,linestyle='-')
-    ax.xaxis.grid(use_grid,which='minor',color=gridcolor,alpha=0.075,linestyle='-')
+    if use_grid:
+        ax.yaxis.grid(use_grid,which='major',color=gridcolor,alpha=0.15,linestyle='-')
+        ax.xaxis.grid(use_grid,which='major',color=gridcolor,alpha=0.15,linestyle='-')
+        ax.yaxis.grid(use_grid,which='minor',color=gridcolor,alpha=0.075,linestyle='-')
+        ax.xaxis.grid(use_grid,which='minor',color=gridcolor,alpha=0.075,linestyle='-')
     labels = ax.get_xticklabels()
     setp(labels,rotation=rotation[0],fontsize=10)
     if y:
