4604c627bf5df5fa2c7e9ba5a5e31a3d2ba3325b
Alex <guinneak198@github.com>
2021-02-15 12:07:37 -0500

    working on lmfit adaptation

--- examples/example_fit_multi_datasets.py
+++ examples/example_fit_multi_datasets.py
@@ -12,7 +12,8 @@ TODO: this should be using the Model interface / built-in models!
 """
 import matplotlib.pyplot as plt
 import numpy as np
-
+from pyspecdata import *
+import sympy as sp
 from lmfit import Parameters, minimize, report_fit
 
 
@@ -45,7 +46,7 @@ def objective(params, x, data):
 ###############################################################################
 # Create five simulated Gaussian data sets
 
-x = np.linspace(-1, 2, 151)
+x = nddata(np.linspace(-1, 2, 151),'x')
 data = []
 for _ in np.arange(5):
     params = Parameters()
