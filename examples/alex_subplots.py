2f405501fabb67fa30d6471d690193f67098f44a
Alex <guinneak198@github.com>
2021-02-19 14:44:11 -0500

    added in repeats dimension

--- examples/alex_subplots.py
+++ examples/alex_subplots.py
@@ -25,10 +25,11 @@ ax1.xaxis.set_ticks_position('top')
 ax4.set_xlabel('this is the x axis')
 #}}}
 #{{{labeling inner dimensions for independent plots
-ax4.set_ylabel('ph1=0')
-ax3.set_ylabel('ph1 =1')
-ax2.set_ylabel('ph1=0')
-ax1.set_ylabel('ph1=1')
+ax4.set_ylabel('ph1=0\nrepeats')
+ax3.set_ylabel('ph1=1\nrepeats')
+ax2.set_ylabel('ph1=0\nrepeats')
+ax1.set_ylabel('ph1=1\nrepeats')
+
 #}}}
 #{{{adding outer dimension labels
 fig.text(0.05,0.25,'ph2=0\n------------------------',ha='center',va='bottom',rotation='vertical')
