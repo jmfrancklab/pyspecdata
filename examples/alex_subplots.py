988c74eac26617659a81a9b60e73cbb4abf6a6e2
John Franck (laptop) <jmfranck@syr.edu>
2021-02-19 17:32:54 -0500

    condensed into a loop

--- examples/alex_subplots.py
+++ examples/alex_subplots.py
@@ -9,58 +9,35 @@ ax2 = plt.subplot(612)
 ax3 = plt.subplot(614)
 ax4 = plt.subplot(615)
 fig.subplots_adjust(hspace=0.8)
+list_of_axes = [ax1,ax2,ax3,ax4]
 #}}}
 #{{{adjusting tick marks and labels
-labels = [item.get_text() for item in ax1.get_xticklabels()]
-empty_string_labels = ['']*len(labels)
-ax1.set_xticklabels(empty_string_labels)
-ax2.set_xticks([])
-ax3.set_xticks([])
-defaultMajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
-defaultMinorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax4.xaxis.set_major_locator(defaultMajorLocator())
-#for the minor ticks, use no labels; default NullFormatter
-ax4.xaxis.set_minor_locator(defaultMinorLocator())
 
-y1MajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
-y1MinorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax1.yaxis.set_major_locator(y1MajorLocator())
-ax1.yaxis.set_minor_locator(y1MinorLocator())
-ax1.yaxis.set_ticks_position('both')
-y2MajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
-y2MinorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax2.yaxis.set_major_locator(y2MajorLocator())
-ax2.yaxis.set_minor_locator(y2MinorLocator())
-ax2.yaxis.set_ticks_position('both')
-y3MajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
-y3MinorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax3.yaxis.set_major_locator(y3MajorLocator())
-ax3.yaxis.set_minor_locator(y3MinorLocator())
-ax3.yaxis.set_ticks_position('both')
-y4MajorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
-y4MinorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax4.yaxis.set_major_locator(y4MajorLocator())
-ax4.yaxis.set_minor_locator(y4MinorLocator())
-ax4.yaxis.set_ticks_position('both')
+yMajorLocator = lambda: mticker.MaxNLocator(steps=[1,2,5,10])
 majorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
 minorLocator = lambda: mticker.AutoMinorLocator(n=5)
-ax1.xaxis.set_major_locator(majorLocator())
-#for the minor ticks, use no labels; default NullFormatter
-ax1.xaxis.set_minor_locator(minorLocator())
-ax1.xaxis.tick_top()
-
-
+for j,thisaxes in enumerate(list_of_axes):
+    thisaxes.set_ylabel('repeats')
+    thisaxes.yaxis.set_minor_locator(minorLocator())
+    thisaxes.yaxis.set_ticks_position('both')
+    if j == 0:
+        thisaxes.xaxis.set_major_locator(majorLocator())
+        #for the minor ticks, use no labels; default NullFormatter
+        thisaxes.xaxis.set_minor_locator(minorLocator())
+        thisaxes.xaxis.tick_top()
+        labels = [item.get_text() for item in thisaxes.get_xticklabels()]
+        empty_string_labels = ['']*len(labels)
+        thisaxes.set_xticklabels(empty_string_labels)
+    elif j == len(list_of_axes)-1:
+        thisaxes.set_xlabel('this is the x axis')
+        thisaxes.xaxis.set_major_locator(majorLocator())
+        thisaxes.xaxis.set_minor_locator(minorLocator())
+    else:
+        thisaxes.set_xticks([])
 #}}}
-#{{{x axis label is same for all plots
-ax4.set_xlabel('this is the x axis')
-#}}}
-#{{{labeling repeats for independent plots
-ax4.set_ylabel('repeats')
-ax3.set_ylabel('repeats')
-ax2.set_ylabel('repeats')
-ax1.set_ylabel('repeats')
 
-#}}}
+# for the following, use the transforms
+
 #{{{labeling ph1 for independent plots
 fig.text(0.07,0.25,'ph1=0',ha='center',va='bottom',rotation='vertical')
 fig.text(0.07,0.38,'ph1=1',ha='center',va='bottom',rotation='vertical')
