77796b1908fbb94207be51751ac09148ca90ebe1
Alex <guinneak198@github.com>
2021-02-21 15:04:30 -0500

    separate lines for each ph1

--- examples/alex_subplots.py
+++ examples/alex_subplots.py
@@ -50,25 +50,43 @@ fig.text(0.06,0.75,'ph2=1',ha='center',va='bottom',rotation='vertical')
 #}}}
 #{{{Adding lines to separate ph1 and repeats
 x1,y1 = ax1.transAxes.transform(r_[0,1])
-x2,y2 = ax2.transAxes.transform(r_[0,0])
+x15,y15 = ax1.transAxes.transform(r_[0,0])
+x2,y2 = ax2.transAxes.transform(r_[0,1])
+x25,y25 = ax2.transAxes.transform(r_[0,0])
 x3,y3 = ax3.transAxes.transform(r_[0,1])
-x4,y4 = ax4.transAxes.transform(r_[0,0])
+x35,y35 = ax3.transAxes.transform(r_[0,0])
+x4,y4 = ax4.transAxes.transform(r_[0,1])
+x45,y45 = ax4.transAxes.transform(r_[0,0])
 x1-=40
 x_text = x1-10
+x15-=40
 x2-=40
+x25-=40
 x1,y1 = fig.transFigure.inverted().transform(r_[x1,y1])
 x_text,_ = fig.transFigure.inverted().transform(r_[x_text,0])
+x15,y15 = fig.transFigure.inverted().transform(r_[x15,y15])
 x2,y2 = fig.transFigure.inverted().transform(r_[x2,y2])
+x25,y25 = fig.transFigure.inverted().transform(r_[x25,y25])
 x3-=40
 x3_text = x3-10
+x35-=40
 x4-=40
+x45-=40
 x3,y3 = fig.transFigure.inverted().transform(r_[x3,y3])
+x35,y35 = fig.transFigure.inverted().transform(r_[x35,y35])
 x4_text,_ = fig.transFigure.inverted().transform(r_[x3_text,0])
 x4,y4 = fig.transFigure.inverted().transform(r_[x4,y4])
-lineA = lines.Line2D([x1,x2],[y1,y2],
+x45,y45 = fig.transFigure.inverted().transform(r_[x45,y45])
+lineA = lines.Line2D([x1,x15],[y1,y15],
         linewidth=3, color='k', transform=fig.transFigure,
         clip_on=False)
-lineC = lines.Line2D([x3,x4],[y3,y4],
+lineA1 = lines.Line2D([x2,x25],[y2,y25],
+        linewidth=3,color='k',transform=fig.transFigure,
+        clip_on=False)
+lineC = lines.Line2D([x3,x35],[y3,y35],
+        linewidth=3,color='k',transform=fig.transFigure,
+        clip_on=False)
+lineC1 = lines.Line2D([x4,x45],[y4,y45],
         linewidth=3,color='k',transform=fig.transFigure,
         clip_on=False)
 #}}}
@@ -96,8 +114,11 @@ lineD = lines.Line2D([x13,x14],[y13,y14],
         clip_on=False)
 #}}}
 fig.add_artist(lineA)
+fig.add_artist(lineA1)
 fig.add_artist(lineB)
 fig.add_artist(lineC)
+fig.add_artist(lineC1)
 fig.add_artist(lineD)
+
 show()
 
