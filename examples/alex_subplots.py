c80030fee6eb2e289d816fd7d045cb0f0e3f3880
Alex <guinneak198@github.com>
2021-02-22 15:25:44 -0500

    brackets in place of arrows

--- examples/alex_subplots.py
+++ examples/alex_subplots.py
@@ -48,111 +48,26 @@ for j,thisaxes in enumerate(list_of_axes):
 # for the following, use the transforms
 
 #{{{labeling ph1 for independent plots
-fig.text(0.14,0.145,'ph1=0',ha='center',va='bottom',rotation='vertical')
-fig.text(0.14,0.37,'ph1=1',ha='center',va='bottom',rotation='vertical')
-fig.text(0.14,0.65,'ph1=0',ha='center',va='bottom',rotation='vertical')
-fig.text(0.14,0.82,'ph1=1',ha='center',va='bottom',rotation='vertical')
+fig.text(0.12,0.145,'ph1=0',ha='center',va='bottom',rotation='vertical')
+fig.text(0.12,0.37,'ph1=1',ha='center',va='bottom',rotation='vertical')
+fig.text(0.12,0.65,'ph1=0',ha='center',va='bottom',rotation='vertical')
+fig.text(0.12,0.82,'ph1=1',ha='center',va='bottom',rotation='vertical')
 #}}}
 #{{{adding outer dimension labels
-fig.text(0.12,0.26,'ph2=0',ha='center',va='bottom',rotation='vertical')
-fig.text(0.12,0.75,'ph2=1',ha='center',va='bottom',rotation='vertical')
+fig.text(0.05,0.26,'ph2=0',ha='center',va='bottom',rotation='vertical')
+fig.text(0.05,0.75,'ph2=1',ha='center',va='bottom',rotation='vertical')
 #}}}
-#{{{Adding lines to separate ph1 and repeats
-x1,y1 = list_of_axes[3].transAxes.transform(r_[0,1])
-x15,y15 = list_of_axes[3].transAxes.transform(r_[0,0])
-x2,y2 = list_of_axes[2].transAxes.transform(r_[0,1])
-x25,y25 = list_of_axes[2].transAxes.transform(r_[0,0])
-x3,y3 = list_of_axes[1].transAxes.transform(r_[0,1])
-x35,y35 = list_of_axes[1].transAxes.transform(r_[0,0])
-x4,y4 = list_of_axes[0].transAxes.transform(r_[0,1])
-x45,y45 = list_of_axes[0].transAxes.transform(r_[0,0])
-x1-=40
-x_text = x1-10
-x15-=40
-x2-=40
-x25-=40
-x1,y1 = fig.transFigure.inverted().transform(r_[x1,y1])
-x_text,_ = fig.transFigure.inverted().transform(r_[x_text,0])
-x15,y15 = fig.transFigure.inverted().transform(r_[x15,y15])
-x2,y2 = fig.transFigure.inverted().transform(r_[x2,y2])
-x25,y25 = fig.transFigure.inverted().transform(r_[x25,y25])
-x3-=40
-x3_text = x3-10
-x35-=40
-x4-=40
-x45-=40
-x3,y3 = fig.transFigure.inverted().transform(r_[x3,y3])
-x35,y35 = fig.transFigure.inverted().transform(r_[x35,y35])
-x4_text,_ = fig.transFigure.inverted().transform(r_[x3_text,0])
-x4,y4 = fig.transFigure.inverted().transform(r_[x4,y4])
-x45,y45 = fig.transFigure.inverted().transform(r_[x45,y45])
-
-lineA = lines.Line2D([x1,x15],[y1,y15],linestyle='--',
-        linewidth=1.5, color='k', transform=fig.transFigure,
-        clip_on=False)
-lineA1 = lines.Line2D([x2,x25],[y2,y25],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-lineC = lines.Line2D([x3,x35],[y3,y35],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-lineC1 = lines.Line2D([x4,x45],[y4,y45],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-#}}}
-#{{{separating ph2 and ph1
-x11,y11 = list_of_axes[3].transAxes.transform(r_[0,1])
-x12,y12 = list_of_axes[2].transAxes.transform(r_[0,0])
-x13,y13 = list_of_axes[1].transAxes.transform(r_[0,1])
-x14,y14 = list_of_axes[0].transAxes.transform(r_[0,0])
-x11-=65
-x1_text = x11-10
-x12-=65
-x11,y11 = fig.transFigure.inverted().transform(r_[x11,y11])
-x1_text,_ = fig.transFigure.inverted().transform(r_[x1_text,0])
-x12,y12 = fig.transFigure.inverted().transform(r_[x12,y12])
-x13-=65
-x13_text = x13-10
-x14-=65
-x13,y13 = fig.transFigure.inverted().transform(r_[x13,y13])
-x14_text,_ = fig.transFigure.inverted().transform(r_[x13_text,0])
-x14,y14 = fig.transFigure.inverted().transform(r_[x14,y14])
-lineB = lines.Line2D([x11,x12],[y11,y12], linestyle='--',linewidth=1.5, color='k', transform=fig.transFigure,
-        clip_on=False)
-lineD = lines.Line2D([x13,x14],[y13,y14], linestyle='--',
-        linewidth=1.5, color='k',transform=fig.transFigure,
-        clip_on=False)
-#}}}
-def make_arrow(ax,x1,y1,x2,y2):
-    trans = ax.get_xaxis_transform()
-    ax.annotate("",(x1,y1),(x2,y2),
-            xycoords="figure fraction",ha="right", va="center",size=8,
-            arrowprops=dict(arrowstyle='simple',fc='k',ec='k'),
-            bbox=dict(boxstyle='square',fc='white'),
-            annotation_clip=False)
-
-#{{{making arrows for ph1
-make_arrow(list_of_axes[0],0.14,0.128,0.14,0.155)
-make_arrow(list_of_axes[0],0.14,0.23,0.14,0.203)
-make_arrow(list_of_axes[1],0.14,0.35,0.14,0.377)
-make_arrow(list_of_axes[1],0.14,0.458,0.14,0.428)
-make_arrow(list_of_axes[2],0.14,0.63,0.14,0.657)
-make_arrow(list_of_axes[2],0.14,0.736,0.14,0.709)
-make_arrow(list_of_axes[3],0.14,0.797,0.14,0.824)
-make_arrow(list_of_axes[3],0.14,0.902,0.14,0.875)
-#}}}
-###{making arrows for ph2
-
-make_arrow(list_of_axes[0],0.12,0.128,0.12,0.25)
-make_arrow(list_of_axes[1],0.12,0.458,0.12,0.335)
-make_arrow(list_of_axes[2],0.12,0.63,0.12,0.75)
-make_arrow(list_of_axes[3],0.12,0.902,0.12,0.81)
-fig.add_artist(lineA)
-fig.add_artist(lineA1)
-fig.add_artist(lineB)
-fig.add_artist(lineC)
-fig.add_artist(lineC1)
-fig.add_artist(lineD)
-
+plt.annotate(r"$\{$",fontsize=74,
+        xy=(0.125,0.15),xycoords='figure fraction')
+plt.annotate(r"$\{$",fontsize=74,
+        xy=(0.125,0.365),xycoords='figure fraction')
+plt.annotate(r"$\{$",fontsize=160,
+        xy=(0.05,0.22),xycoords='figure fraction')
+plt.annotate(r"$\{$",fontsize=74,
+        xy=(0.125,0.64),xycoords='figure fraction')
+plt.annotate(r"$\{$",fontsize=74,
+        xy=(0.125,0.815),xycoords='figure fraction')
+plt.annotate(r"$\{$",fontsize=150,
+        xy=(0.05,0.7),xycoords='figure fraction')
 show()
 
