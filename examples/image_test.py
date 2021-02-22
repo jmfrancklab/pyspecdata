db05846faf2c51540899bc27e0c54ada35e6c9e9
Alec Beaton (laptop) <aabeaton@syr.edu>
2021-02-22 11:33:51 -0500

    Start integrating AG code, working example

--- examples/image_test.py
+++ examples/image_test.py
@@ -5,7 +5,7 @@ from pyspecdata import *
 t_axis = nddata(r_[0:2:2048j],'t2')
 s = exp(1j*2*pi*5*t_axis - t_axis/800e-3 )
 s += exp(1j*2*pi*-30*t_axis - t_axis/800e-3)
-ph1 = nddata(r_[0:8]/4.,'ph1')
+ph1 = nddata(r_[0:4]/4.,'ph1')
 ph2 = nddata(r_[0,2]/4.,'ph2')
 # this cannot start at 0 since we multiply s by it
 repeats = nddata(r_[1:6],'repeats')
@@ -26,7 +26,7 @@ print(A.ndim)
 
 # begin new code
 # determine list of divisions
-# still needs to be put into algorithm for more than 3 dim
+# may need work to become fully 'algorithmic'
 div_list = [1]
 #div_list = (div_list + [2])*(A.shape[-1*A.ndim] - 2) + div_list
 div_list = (div_list )*(A.shape[-1*A.ndim + 1] - 2) + div_list
@@ -44,7 +44,6 @@ for dim_idx in range(A.ndim):
         num_axes_obj *= A.shape[counter]
 print(num_axes_obj)
 axes_list = [[1.] * 4] * num_axes_obj
-#axes_list = zeros(num_axes_obj,4)
 print(axes_list)
 print(len(axes_list))
 axes_list = np.array(axes_list)
@@ -59,8 +58,7 @@ bottom_border = 0.1
 left_border = 0.1
 right_border = 0.1
 
-#div_list.insert(0,0)
-division_scale = 0.01
+division_scale = 0.03
 division_space = sum(div_list) * division_scale
 
 height = (1. - (top_border+bottom_border+division_space))/float(num_axes_obj)
@@ -68,7 +66,6 @@ width = 1. - (left_border+right_border)
 
 div_counter = 0
 print(div_list)
-
 
 for outer_index in range(A.shape[-1*A.ndim]):
     for inner_index in range(A.shape[-1*A.ndim + 1]):
@@ -86,9 +83,46 @@ for outer_index in range(A.shape[-1*A.ndim]):
             div_counter += 1
 # end newer code
 
+print(axes_list[0][0])
+
+print(A.dimlabels[-1*A.ndim])
+print(A.dimlabels[-1*A.ndim + 1])
+
+# try integrating code from alex_subplots
+
+yMajorLocator = lambda: mticker.MaxNLocator(steps=[1,2,5,10])
+majorLocator = lambda: mticker.MaxNLocator(min_n_ticks=4, steps=[1,2,5,10])
+minorLocator = lambda: mticker.AutoMinorLocator(n=5)
+for outer_index in range(A.shape[-1*A.ndim]):
+    for inner_index in range(A.shape[-1*A.ndim + 1]):
+        print(outer_index,inner_index)
+
+
+    #thisaxes.set_ylabel('repeats')
+    #thisaxes.yaxis.set_minor_locator(minorLocator())
+    #thisaxes.yaxis.set_ticks_position('both')
+    #if j == len(list_of_axes)-1:
+    #    thisaxes.xaxis.set_major_locator(majorLocator())
+    #    #for the minor ticks, use no labels; default NullFormatter
+    #    thisaxes.xaxis.set_minor_locator(minorLocator())
+    #    thisaxes.xaxis.tick_top()
+    #    labels = [item.get_text() for item in thisaxes.get_xticklabels()]
+    #    empty_string_labels = ['']*len(labels)
+    #    thisaxes.set_xticklabels(empty_string_labels)
+    #elif j == 0:
+    #    thisaxes.set_xlabel('this is the x axis')
+    #    thisaxes.xaxis.set_major_locator(majorLocator())
+    #    thisaxes.xaxis.set_minor_locator(minorLocator())
+    #else:
+    #    thisaxes.set_xticks([])
 for outer_index in range(A.shape[-1*A.ndim]):
     for inner_index in range(A.shape[-1*A.ndim + 1]):
         temp = list(axes_list[outer_index,inner_index])
+        this_ax = axes(temp)
+        this_ax.set_ylabel(A.dimlabels[-2])
+        this_ax.yaxis.set_minor_locator(minorLocator())
         figure(1);
-        image(s['ph1',outer_index]['ph2',inner_index],ax=axes(temp))
+        image(s['ph1',outer_index]['ph2',inner_index],ax=this_ax)
+        axes(temp).yaxis.set_ticks_position('both')
+        
 show();quit()
