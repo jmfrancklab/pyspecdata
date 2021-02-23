dc6c30ff38da56c01125f57249598469e96b9c2e
Alex <guinneak198@github.com>
2021-02-23 12:01:24 -0500

    working on adding in brackets from bezier.py

--- examples/alex_subplots.py
+++ examples/alex_subplots.py
@@ -2,13 +2,30 @@ from pyspecdata import *
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.lines as lines
-#import matplotlib.patches
-##import matplotlib.patches.ArrowStyle
-#import matplotlib.patches.ArrowStyle.get_styles
-#import matplotlib.axes.Axes.annotate
-#{{{generating subplots with space between
-bottoms = [0.13, 0.35, 0.63, 0.8] # this is a semi-random set of numbers -- in
-#                                   reality, this would come from Alec's code
+from matplotlib.path import Path
+import matplotlib.patches as patches
+
+bottoms = [0.13, 0.35, 0.63, 0.8]
+verts = [
+   (0., 0.),   # start
+   (0.1, 0.),  # ctrl
+   (0.1, 0.5),  # end
+   (0.1, 1.0),  # ctrl
+   (0.2, 1.0),  # end
+   (0.25, 1.0),  # ctrl
+   (0.25, 1.0),  # end
+]
+
+codes = [
+    Path.MOVETO,# start
+    Path.CURVE3, # ctrl
+    Path.CURVE3, # end
+    Path.CURVE3, # ctrl
+    Path.CURVE3, # end
+    Path.CURVE3, # ctrl
+    Path.CURVE3, # end
+]
+
 height = 0.1
 fig=plt.figure(1)
 list_of_axes = []
@@ -55,12 +72,12 @@ fig.text(0.14,0.65,'ph1=0',ha='center',va='bottom',rotation='vertical')
 fig.text(0.14,0.82,'ph1=1',ha='center',va='bottom',rotation='vertical')
 #}}}
 #{{{adding outer dimension labels
-fig.text(0.12,0.26,'ph2=0',ha='center',va='bottom',rotation='vertical')
-fig.text(0.12,0.75,'ph2=1',ha='center',va='bottom',rotation='vertical')
+fig.text(0.07,0.25,'ph2=0',ha='center',va='bottom',rotation='vertical')
+fig.text(0.07,0.725,'ph2=1',ha='center',va='bottom',rotation='vertical')
 #}}}
 #{{{Adding lines to separate ph1 and repeats
-x1,y1 = list_of_axes[3].transAxes.transform(r_[0,0])
-x15,y15 = list_of_axes[3].transAxes.transform(r_[1,0])
+x1,y1 = list_of_axes[3].transAxes.transform(r_[0,1])
+x15,y15 = list_of_axes[3].transAxes.transform(r_[0,0])
 x2,y2 = list_of_axes[2].transAxes.transform(r_[0,1])
 x25,y25 = list_of_axes[2].transAxes.transform(r_[0,0])
 x3,y3 = list_of_axes[1].transAxes.transform(r_[0,1])
@@ -88,28 +105,16 @@ x4_text,_ = fig.transFigure.inverted().transform(r_[x3_text,0])
 x4,y4 = fig.transFigure.inverted().transform(r_[x4,y4])
 x45,y45 = fig.transFigure.inverted().transform(r_[x45,y45])
 
-lineA = lines.Line2D([0.12,0.15],[0.8,0.8],linestyle='--',
+lineA = lines.Line2D([x1,x15],[y1,y15],linestyle='--',
         linewidth=1.5, color='k', transform=fig.transFigure,
         clip_on=False)
-lineA1 = lines.Line2D([0.12,0.17],[0.9,0.9],linestyle='--',
+lineA1 = lines.Line2D([x2,x25],[y2,y25],linestyle='--',
         linewidth=1.5,color='k',transform=fig.transFigure,
         clip_on=False)
-lineB = lines.Line2D([0.12,0.15],[0.735,0.735],linestyle='--',
-        linewidth=1.5, color='k',transform=fig.transFigure,
-        clip_on=False)
-lineB1 = lines.Line2D([0.12,0.17],[0.63,0.63],linestyle='--',
+lineC = lines.Line2D([x3,x35],[y3,y35],linestyle='--',
         linewidth=1.5,color='k',transform=fig.transFigure,
         clip_on=False)
-lineC = lines.Line2D([0.12,0.15],[0.35,0.35],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-lineC1 = lines.Line2D([0.12,0.17],[0.46,0.46],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-lineD = lines.Line2D([0.12,0.15],[0.23,0.23],linestyle='--',
-        linewidth=1.5,color='k',transform=fig.transFigure,
-        clip_on=False)
-lineD1 = lines.Line2D([0.12,0.17],[0.13,0.13],linestyle='--',
+lineC1 = lines.Line2D([x4,x45],[y4,y45],linestyle='--',
         linewidth=1.5,color='k',transform=fig.transFigure,
         clip_on=False)
 #}}}
@@ -130,38 +135,38 @@ x14-=65
 x13,y13 = fig.transFigure.inverted().transform(r_[x13,y13])
 x14_text,_ = fig.transFigure.inverted().transform(r_[x13_text,0])
 x14,y14 = fig.transFigure.inverted().transform(r_[x14,y14])
+lineB = lines.Line2D([x11,x12],[y11,y12], linestyle='--',linewidth=1.5, color='k', transform=fig.transFigure,
+        clip_on=False)
+lineD = lines.Line2D([x13,x14],[y13,y14], linestyle='--',
+        linewidth=1.5, color='k',transform=fig.transFigure,
+        clip_on=False)
 #}}}
-def make_arrow(ax,x1,y1,x2,y2):
-    trans = ax.get_xaxis_transform()
-    ax.annotate("",(x1,y1),(x2,y2),
-            xycoords="figure fraction",ha="right", va="center",size=8,
-            arrowprops=dict(arrowstyle='simple',fc='k',ec='k'),
-            bbox=dict(boxstyle='square',fc='white'),
-            annotation_clip=False)
-
-#{{{making arrows for ph1
-make_arrow(list_of_axes[0],0.14,0.128,0.14,0.155)
-make_arrow(list_of_axes[0],0.14,0.23,0.14,0.203)
-make_arrow(list_of_axes[1],0.14,0.35,0.14,0.377)
-make_arrow(list_of_axes[1],0.14,0.458,0.14,0.428)
-make_arrow(list_of_axes[2],0.14,0.63,0.14,0.657)
-make_arrow(list_of_axes[2],0.14,0.736,0.14,0.709)
-make_arrow(list_of_axes[3],0.14,0.797,0.14,0.824)
-make_arrow(list_of_axes[3],0.14,0.902,0.14,0.875)
+#{{{functions for brackets
+def gen_curly(verts):
+    path = Path(verts, codes)
+    patch = patches.PathPatch(path, facecolor='none', lw=2)
+    fig.add_artist(patch)
+    xs, ys = zip(*verts)
+    # following shown for a guide -- don't use in final version
+    fig.add_artist(lines.Line2D(xs, ys, linestyle='--', lw=2, color='black', ms=10,alpha=0.1))
+def draw_bracket(x_left,y_middle,y_height,x_width=0.25):
+    gen_curly([(x/0.25*x_width+x_left,y*y_height/2+y_middle) for x,y in verts])
+    gen_curly([(x/0.25*x_width++x_left,-y*y_height/2+y_middle) for x,y in verts])
+def add_label(x,y,thetext):
+    # {{{ push out by 12 pts
+    x,y = fig.transFigure.transform((x,y))
+    x -= 12 
+    x,y = fig.transFigure.inverted().transform((x,y))
+    # }}}
+    fig.text(x,y,thetext,
+            va='center',ha='center',
+            rotation=90,
+            transform=fig.transFigure)
 #}}}
-###{making arrows for ph2
-
-make_arrow(list_of_axes[0],0.12,0.128,0.12,0.25)
-make_arrow(list_of_axes[1],0.12,0.458,0.12,0.335)
-make_arrow(list_of_axes[2],0.12,0.63,0.12,0.75)
-make_arrow(list_of_axes[3],0.12,0.902,0.12,0.81)
-fig.add_artist(lineA)
-fig.add_artist(lineA1)
-fig.add_artist(lineB)
-fig.add_artist(lineB1)
-fig.add_artist(lineC)
-fig.add_artist(lineC1)
-fig.add_artist(lineD)
-fig.add_artist(lineD1)
+#{{{drawing brackets on figure
+draw_bracket(0.08,y4+0.05,0.315,x_width=0.04)
+draw_bracket(0.08,y2+0.03,0.28,x_width=0.04)
+draw_bracket(0.12,y45+0.05,0.12,x_width=0.02)
+#}}}
 show()
 
