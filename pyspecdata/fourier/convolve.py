3b26f3f3bda2c187fbf71691c80390d182264af8
Alec Beaton (laptop) <aabeaton@syr.edu>
2021-07-30 17:44:04 -0400

    change print to log, take inverse of filter width, and roll back of documentation

--- pyspecdata/fourier/convolve.py
+++ pyspecdata/fourier/convolve.py
@@ -1,10 +1,12 @@
 from ..general_functions import inside_sphinx
 from pylab import r_,fft,ifft,ifftshift,fftshift,exp,ones_like
+from pyspecdata import init_logging,strm
+logger = init_logging("debug")
 
 def convolve(self,axisname,filterwidth,convfunc = (lambda x,y: exp(-(x**2)/(2.0*(y**2))))):
-    r'''Perform a convolution, by multiplying
-    current dataset in its conjugate domain by the
-    convolution function.
+    r'''Perform a convolution on the data in its
+    current domain via multiplication in its
+    conjugate domain.
     
     Parameters
     ==========
@@ -13,20 +15,18 @@ def convolve(self,axisname,filterwidth,convfunc = (lambda x,y: exp(-(x**2)/(2.0*
         apply the convolution along `axisname`
 
     filterwidth: double
-        width of the convolution function (specify
-        in the conjugate domain, since this is
-        where the convolution function is
-        constructed)
-        
+        width of the convolution function
+        (this value must be provided in the same
+        domain as that in which the data exists
+        when you call this function on said data)
 
     convfunc: function
-        A function defined in the conjugate
-        Fourire domain of self that takes two
+        A function that takes two
         arguments -- the first are the axis
         coordinates and the second is
-        `filterwidth`.  Default is a normalized
-        Gaussian of width (:math:`\sigma`)
-        `filterwidth`
+        `filterwidth` (see `filterwidth`).
+        Default is a normalized Gaussian of width
+        (:math:`\sigma`) `filterwidth`
         :math:`\frac{1}{2 \sigma^2}\exp\left( - \frac{x^2}{2 \sigma^2} \right)`
         For example if you want a complex Lorentzian with `filterwidth` controlled by the rate :math:`R`, 
         *i.e.*
@@ -36,24 +36,24 @@ def convolve(self,axisname,filterwidth,convfunc = (lambda x,y: exp(-(x**2)/(2.0*
     time_domain = True
     if self.get_ft_prop(axisname):
         # detect self in frequency domain
-        print("detect self in frequency domain")
+        logger.info(strm("detect self in frequency domain"))
         self.ift(axisname)
         time_domain = False
     elif self.get_ft_prop(axisname,['start','freq']) is None:
         # detect self in time domain, never FT'd
-        print("detect self in time domain, never FT'd")
+        logger.info(strm("detect self in time domain, never FT'd"))
         self.ft(axisname, shift=True)
     else:
         # detect self in time domain, already FT'd
-        print("detect self in time domain, already FT'd")
+        logger.info(strm("detect self in time domain, already FT'd"))
         self.ft(axisname)
     x = self.fromaxis(axisname)
-    myfilter = convfunc(x,filterwidth)
+    myfilter = convfunc(x,1/filterwidth)
     newdata = self*myfilter
     self.data = newdata.data
     if time_domain:
         self.ift(axisname)
     else:
         self.ft(axisname)
-    print("before return",axisname,self.get_ft_prop(axisname))
+    logger.info(strm("before return",axisname,self.get_ft_prop(axisname)))
     return self
