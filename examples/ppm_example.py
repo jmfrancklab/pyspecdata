ff9f36a8545091de09d6781b67743453c9ce9da2
Alec Beaton (laptop) <aabeaton@syr.edu>
2021-10-12 15:34:42 -0400

    Update for new sample data containing TMS

--- examples/ppm_example.py
+++ examples/ppm_example.py
@@ -1,5 +1,6 @@
 from pylab import *
 from pyspecdata import *
+#pull parameters from RM1_sept30_1H_211001 expno 1
 
 # make peaks on a frequency axis
 x = nddata(r_[0:6.8157439:32768j], "t2")
@@ -8,9 +9,9 @@ x.set_units("t2", "s")
 # generate time-domain signal
 SW_h = 1./(x.getaxis('t2')[1] - x.getaxis('t2')[0])
 signal = 0 * x  # create an array of zeros that's the right shape
-SFO1 = 400.130438262389
-BF1 = 400.1
-TMS_shift = 12.5
+SFO1 = 400.1307334
+BF1 = 400.13
+TMS_shift = 0.
 for A, nu, R in [
     (0.1, TMS_shift, 0.04),
     (1, 25, 0.08),
@@ -24,8 +25,6 @@ for A, nu, R in [
 signal.set_units("t2", "s")
 signal.ft("t2", shift=True)
 
-# Parameters needed by to_ppm to work
-# pulled from BV_polymer_apr27_2021
 SF = SFO1+TMS_shift*1e-6
 OFFSET = (SFO1/SF-1) * 1.0e6 + 0.5 * SW_h * SFO1/SF
 signal.set_prop('acq',{'SFO1':SFO1,'SW_h':SW_h,'O1':438.262389})
