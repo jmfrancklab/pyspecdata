d300ac5e402060d432e1d2882077cecb40ceb0d6
John Franck (laptop) <jmfranck@syr.edu>
2021-10-20 20:57:36 -0400

    make sure I can build docs

--- examples/text_only/phase_cycling_labels.py
+++ examples/text_only/phase_cycling_labels.py
@@ -1,3 +1,9 @@
+"""
+phase cycling labels
+====================
+
+demonstrate automatic labeling of phase cycling (coherence) axes
+"""
 from pyspecdata import *
 a = nddata(r_[0:90],[3,3,10],['ph1','ph2','t']).setaxis('ph1','#').setaxis('ph2','#').setaxis('t','#').set_units('t','s')
 a.ft(['ph1','ph2','t'])
