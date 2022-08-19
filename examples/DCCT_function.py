142f22f44874abfb8348f4b29a77b018cc846c19
Alex <guinneak198@github.com>
2022-08-19 15:03:20 -0400

    bel_spacing

--- examples/DCCT_function.py
+++ examples/DCCT_function.py
@@ -59,7 +59,7 @@ with figlist_var() as fl:
     # keyword arguments to use throughout
     dcct_kwargs = dict(
         total_spacing=0.25,
-        label_spacing_multiplier=50,
+        label_spacing_multiplier=40,
         LHS_pad=0.05,
         )
     fig = fl.next("raw data")
