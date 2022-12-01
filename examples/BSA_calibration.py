40e8711cc3c1ee4092993592c035a2881791e265
John M. Franck (SU laptop) <jmfranck@syr.edu>
2022-12-01 18:15:38 -0500

    just ran black

--- examples/BSA_calibration.py
+++ examples/BSA_calibration.py
@@ -19,9 +19,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 import sympy as sp
+
 # {{{ some constants
 wv = "$\\lambda$"
-wv_range = (276, 281) # range of wavelengths we consider to be our peak
+wv_range = (276, 281)  # range of wavelengths we consider to be our peak
 # }}}
 
 # HERE we get dictionaries of nddata
@@ -36,56 +37,66 @@ print("the experiments present in this file are:", dataWKBSA.keys())
 # in the protocol.
 #
 # Code below expects that when the label/key below is a concentration, it has a % symbol
-list_of_runs = OrderedDict([
-        ("Water","UP_H2O"),
-        ("0 %","K-PBSbuffer"),
-        ("0.0078 %","J-0p0078%BSA"),
-        ("0.0156 %","H-0p0156%BSA"),
-        ("0.0234 %","G-0p0234%BSA_actual"),
-        ("0.0311 %","F-0p0311%BSA"),
-        ("0.0389 %","E-0p0389%BSA"),
-        ("0.0466 %","D-0p0466%BSA"),
-        ("0.0544 %","C-0p0544%BSA"),
-        ("0.0622 %","B-0p0622%BSA"),
-        ("0.0700 %","A-0p0700%BSA"),
-        ])
-background = "0 %" # this is the label of the thing we want to subtract as the background
+list_of_runs = OrderedDict(
+    [
+        ("Water", "UP_H2O"),
+        ("0 %", "K-PBSbuffer"),
+        ("0.0078 %", "J-0p0078%BSA"),
+        ("0.0156 %", "H-0p0156%BSA"),
+        ("0.0234 %", "G-0p0234%BSA_actual"),
+        ("0.0311 %", "F-0p0311%BSA"),
+        ("0.0389 %", "E-0p0389%BSA"),
+        ("0.0466 %", "D-0p0466%BSA"),
+        ("0.0544 %", "C-0p0544%BSA"),
+        ("0.0622 %", "B-0p0622%BSA"),
+        ("0.0700 %", "A-0p0700%BSA"),
+    ]
+)
+background = (
+    "0 %"  # this is the label of the thing we want to subtract as the background
+)
 with figlist_var() as fl:
     # {{{ first, just show the raw data and the region of the spectrum that we intend to slice
     bg_data = dataWKBSA[list_of_runs[background]]
     fl.next("raw data", legend=True)
-    plt.axvspan(wv_range[0], wv_range[1], color='k', alpha=0.1)
+    plt.axvspan(wv_range[0], wv_range[1], color="k", alpha=0.1)
     for thislabel, thisscan in list_of_runs.items():
-        if thislabel in [background,"Water"]:
-            plot(dataWKBSA[thisscan], label=thislabel+'\n(no background subtraction)',
-                    alpha=0.5)
+        if thislabel in [background, "Water"]:
+            plot(
+                dataWKBSA[thisscan],
+                label=thislabel + "\n(no background subtraction)",
+                alpha=0.5,
+            )
         else:
-            plot(dataWKBSA[thisscan]-bg_data, label=thislabel,
-                    alpha=0.5)
+            plot(dataWKBSA[thisscan] - bg_data, label=thislabel, alpha=0.5)
     # }}}
     # {{{ pull a list of all of the spectra whose labels express a concentration
     #     (percentage), as well as an array of floating point values for the
     #     corresponding concentrations
-    conc_labels = [k for k,v in list_of_runs.items() if '%' in k]
-    conc_values = array([float(j.replace('%','')) for j in conc_labels])
+    conc_labels = [k for k, v in list_of_runs.items() if "%" in k]
+    conc_values = array([float(j.replace("%", "")) for j in conc_labels])
     bg_data = dataWKBSA[list_of_runs[background]]
-    all_data = concat([dataWKBSA[list_of_runs[k]]-bg_data for k in conc_labels],'concentration').setaxis('concentration',conc_values)
+    all_data = concat(
+        [dataWKBSA[list_of_runs[k]] - bg_data for k in conc_labels], "concentration"
+    ).setaxis("concentration", conc_values)
     # }}}
     # {{{ now, gather the data in to a 2D array, so that I can just average the peak and plot the calibration curve
     A280 = all_data[wv:wv_range].mean(wv)
-    fl.next('calibration curve')
-    fl.plot(A280,'o')
-    c = A280.polyfit('concentration', order=1)
-    fl.plot(A280.eval_poly(c, 'concentration'))
+    fl.next("calibration curve")
+    fl.plot(A280, "o")
+    c = A280.polyfit("concentration", order=1)
+    fl.plot(A280.eval_poly(c, "concentration"))
     # }}}
     # {{{ use sympy to print the fit equation
-    conc_symb = sp.symbols('c', real=True)
-    expr = sum([conc_symb**j*sp.Float(c[j],3) for j in range(len(c))])
+    conc_symb = sp.symbols("c", real=True)
+    expr = sum([conc_symb ** j * sp.Float(c[j], 3) for j in range(len(c))])
     plt.text(
-            0.5, 0.5, sp.latex(expr),
-            transform = gca().transAxes,
-            va='center',
-            ha='center',
-            size=20,
-            )
+        0.5,
+        0.5,
+        sp.latex(expr),
+        transform=gca().transAxes,
+        va="center",
+        ha="center",
+        size=20,
+    )
     # }}}
