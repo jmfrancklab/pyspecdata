b29ffdf345a9760a0208c654a4a043445fe00d14
Warren Kincaid <wfkincai@g.syr.edu>
2022-12-02 18:55:26 -0500

    Matching BSA_calibration

--- examples/BSA_calibration.py
+++ examples/BSA_calibration.py
@@ -29,66 +29,6 @@ import sympy as sp
 wv = "$\\lambda$"
 wv_range = (276, 281)  # range of wavelengths we consider to be our peak
 # }}}
-
-# HERE we get dictionaries of nddata
-<<<<<<< HEAD
-dataWKBSA = find_file("221110_BSAexerciseWK_0p07-0percentBSAcalibration.BSW", exp_type="UV_Vis/BSA_calib")
-#print("the experiments present in this file are:", data.keys())
-wv = "$\\lambda$"
-# HERE make a dictionary with keys equal to labels (using concentration,
-# and with values equal to the actual nddata objects for the spectra
-list_of_runs = [
-        {
-            "Water": dataWKBSA["UP_H2O"],
-            "0 %": dataWKBSA["K-PBSbuffer"],
-            "0.0078 %": dataWKBSA["J-0p0078%BSA"],
-            "0.0156 %": dataWKBSA["H-0p0156%BSA"],
-            "0.0234 %": dataWKBSA["G-0p0234%BSA_actual"],
-            "0.0311 %": dataWKBSA["F-0p0311%BSA"],
-            "0.0389 %": dataWKBSA["E-0p0389%BSA"],
-            "0.0466 %": dataWKBSA["D-0p0466%BSA"],
-            "0.0544 %": dataWKBSA["C-0p0544%BSA"],
-            "0.0622 %": dataWKBSA["B-0p0622%BSA"],
-            "0.0700 %": dataWKBSA["A-0p0700%BSA"]
-            }
-        ]
-#Dictionary with labels of each run in the UV file
-# HERE add a dilution factor -- we'll multiply in the final plot
-dilution_factor = [1]
-# HERE give labels of equal length
-label_of_run = ["WK BSA run"]
-finalcurve = [{} for j in range(len(list_of_runs))]  # two dicts, one for each
-A280 = [{} for j in range(len(list_of_runs))]  # two dicts, one for each
-print(range(len(list_of_runs)))
-# }}}
-whichunits = "%"
-#prop_cycle = plt.rcParams["axes.prop_cycle"]
-#prop_cycle = prop_cycle * plt.cycler(alpha=[0.5])
-#print(plt.rcParams["ytick.major.size"])
-#plt.rcParams["ytick.major.size"] = 3
-#print(plt.rcParams["ytick.major.size"])
-#plt.rcParams["font.size"] = 8
-#plt.rcParams["legend.fontsize"] = 8
-for j, thisset in enumerate(list_of_runs):
-    #thecycler = prop_cycle()
-    for k, v in thisset.items():
-        v.name(label_of_run[j])
-        #kw = next(thecycler)
-        thisd = v[wv:(278, None)]
-        if k not in ["Water"]:
-            A280[j][k] = v[wv:(276, 281)].C.mean(wv).item()
-        if k not in [f"0 %", "Water"]:
-            BSA_diff = thisd[wv:(280, None)] - thisset[f"0 %"][wv:(280, None)]
-            finalcurve[j][k] = BSA_diff[wv:(276, 281)].C.mean(wv).item()
-figure()
-for j in range(len(finalcurve)):    
-    thisdil = dilution_factor[j]
-    x, y = zip(*finalcurve[j].items())
-    x = [float(j.replace(f" {whichunits}", "")) for j in x]
-    y = list(y)
-    plot(x, array(y)*thisdil, "o", alpha=0.5, label=f"{label_of_run[j]} baseline_adj_A280")
-    plt.plot(x, y)
-=======
 dataWKBSA = find_file(
     "221110_BSAexerciseWK_0p07-0percentBSAcalibration.BSW", exp_type="UV_Vis/BSA_calib"
 )
@@ -162,5 +102,4 @@ with figlist_var() as fl:
         ha="center",
         size=20,
     )
->>>>>>> 6248cd6a27da3b13c98bead5ede854a3f14addda
     # }}}
