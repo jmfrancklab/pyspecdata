836b3386ef65292cd0bafb05ea5149c45c9ca09b
Alex <guinneak198@github.com>
2023-05-23 18:59:27 -0400

    no more proc_scripts

--- examples/calculate_covariance.py
+++ examples/calculate_covariance.py
@@ -2,22 +2,22 @@
 Calculation of the Covariance Matrix
 ====================================
 After rescaling plots, the covariance matrix is calculated
-and then plotted for a 2D Field experiment
+and then plotted for a 2D Field experiment (spectra as a function
+of field with multiple collections or "Times")
+
 """
-from pyspecProcScripts import *
 from pyspecdata import *
 from pylab import *
 
 fieldaxis = "$B_0$"
 exp_type = "francklab_esr/alex"
 with figlist_var() as fl:
-    for filenum, (thisfile, calibration, diameter) in enumerate(
-        [("230504_3p8mM_TEMPOL_stb_wt_4x.DSC", "230202", "QESR caps")]
+    for filenum, (thisfile) in enumerate(
+        [("230504_3p8mM_TEMPOL_stb_wt_4x.DSC")]
     ):
         d = find_file(thisfile, exp_type=exp_type)["harmonic", 0]
         d.rename("'Time'", "observations")
         d.reorder(["observations", fieldaxis])
-        d /= QESR_scalefactor(d, calibration_name=calibration, diameter_name=diameter)
         d.ift(fieldaxis, shift=True)
         fl.next("Covariance in U domain")
         fl.image(d.C.cov_mat("observations"))
