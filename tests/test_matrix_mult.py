4dda619b49fb918e87e6e76f5a59c0837ef2e05b
John Franck <jmfranck@jmfDebian.syr.edu>
2023-11-14 19:01:20 -0500

    removed test that was not working

--- tests/test_matrix_mult.py
+++ tests/test_matrix_mult.py
@@ -16,20 +16,5 @@ class TestMatrixMultiplication(unittest.TestCase):
 
         # Assert that the result is as expected
         np.testing.assert_array_almost_equal(result.data, expected_result.data)
-    # now, include a second function that performs a similar tests, but where
-    # the dimensions are not the same, and where we use "along" to specify how to
-    # multiply
-    def test_matrix_multiplication_along(self):
-        # Create two matrices
-        matrix1 = nddata(np.array([1, 2, 3, 4]), (2, 2), ['a', 'b'])
-        matrix2 = nddata(np.array([5, 6, 7, 8]), (2, 2), ['y', 'x'])
-        # use along to specify how to multiply
-        result = matrix1.along('b') @ matrix2.along('x')
-        # assert that the result is as expected -- as above, use
-        # numpy multiplication on the data attributes to check
-        # that the data is the same
-        expected_result = nddata(matrix1.data @ matrix2.data.T, (2, 2), ['a', 'y'])
-        np.testing.assert_array_almost_equal(result.data, expected_result.data)
-
 if __name__ == '__main__':
     unittest.main()
