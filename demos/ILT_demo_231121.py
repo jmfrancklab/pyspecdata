178bcbfc44bb260ebbe3c7b149e29d39b0f32e11
John Franck <jmfranck@jmfDebian.syr.edu>
2023-12-03 16:10:06 -0500

    get ILT demo to work similar to AB's thesis

--- demos/ILT_demo_231121.py
+++ demos/ILT_demo_231121.py
@@ -1,6 +1,7 @@
 # In[1]:
 
 get_ipython().magic(u'load_ext pyspecdata.ipy')
+init_logging(level='debug')
 
 # Here, we're going to provide a few demonstrations of the ILT functionality.  Let's start with Fig 1.10 in A. Beaton's thesis, which is based off the figures
 # in Venkataramanan.
@@ -13,27 +14,29 @@ LT1_name = r'$\log(T_1)$'
 LT1 = nddata(linspace(-2.5,0.5,NT1), LT1_name)
 LT2_name = r'$\log(T_2)$'
 LT2 = nddata(linspace(-2.5,0.3,NT2), LT2_name)
+mu = [-1.25,-1.75]
+sigma = [0.1,0.1]
 exact_data = exp(-(LT1-mu[0])**2/2/sigma[0]**2-(LT2-mu[1])**2/2/sigma[1]**2)
 slanted_coord1 = (LT1+LT2)/sqrt(2)
-slanted_coord2 = (LT1-LT2)/sqrt(2)
-mu = [-1.8,-0.5]
+slanted_coord2 = (LT2-LT1)/sqrt(2)
+mu = [-1.0,-0.4]
 mu = [ # convert to slanted coords
-        sum(mu)/sqrt(2),
-        diff(mu)/sqrt(2)]
-sigma = [0.1,0.15] # in slanted
-exact_data = exp(-(slanted_coord1-mu[0])**2/2/sigma[0]**2-(slanted_coord2-mu[1])**2/2/sigma[1]**2)
+        (mu[0]+mu[1])/sqrt(2),
+        (mu[1]-mu[0])/sqrt(2)]
+sigma = [0.5,0.05] # in slanted
+exact_data += exp(-(slanted_coord1-mu[0])**2/2/sigma[0]**2-(slanted_coord2-mu[1])**2/2/sigma[1]**2)
 exact_data
 
 # In[2]:
 
-tau1 = nddata(logspace(-2.5,0.25+log10(4),30),'tau1')
-tau2 = nddata(linspace(10**-2.5,10**(0.25+log10(4)),30),'tau2')
+tau1 = nddata(logspace(log10(5.0e-4),log10(4),30),'tau1')
+tau2 = nddata(linspace(5.0e-4,3.8,1000),'tau2')
 
 # $$T_1 = 10^{\log(T_1)}$$
 # $$R_1 = 10^{-\log(T_1)}$$
 # $$\ln(R_1) = -\log(T_1) \ln(10)$$
 
-basis = (1-2*exp(-tau1*10**-LT1))*exp(-tau2*10**-LT2)
+basis = exp(-tau2/10**LT2)*(1-2*exp(-tau1/10**LT1))
 basis.shape
 
 # In[3]:
