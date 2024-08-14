490bb658bf9f16ad02dc91069438615bfafa9713
Alex Guinness <aguinnes@syr.edu>
2024-08-14 12:55:29 -0400

    run black and flake

--- examples/indiv_funcs/hdf_test.py
+++ examples/indiv_funcs/hdf_test.py
@@ -1,18 +1,20 @@
-from pylab import *
-from pyspecdata import *
+from pyspecdata import nddata, nddata_hdf5
+from np import r_
 import os
-a = nddata(r_[0:9],[3,3],['a','b']).labels(
-        dict(
-            a = r_[0:3],
-            b = r_[1:4],
-            ))
-a.name('test_data')
-a.set_units('a','s')
-a.hdf5_write('test.h5')
-a_reload = nddata_hdf5('test.h5/test_data')
-print("Units set for the a axis are:", a_reload.get_units('a'))
+
+a = nddata(r_[0:9], [3, 3], ["a", "b"]).labels(
+    dict(
+        a=r_[0:3],
+        b=r_[1:4],
+    )
+)
+a.name("test_data")
+a.set_units("a", "s")
+a.hdf5_write("test.h5")
+a_reload = nddata_hdf5("test.h5/test_data")
+print("Units set for the a axis are:", a_reload.get_units("a"))
 assert all(a.data == a_reload.data)
-assert all(a.getaxis('a') == a_reload.getaxis('a'))
-assert all(a.getaxis('b') == a_reload.getaxis('b'))
+assert all(a.getaxis("a") == a_reload.getaxis("a"))
+assert all(a.getaxis("b") == a_reload.getaxis("b"))
 print(a_reload)
-os.remove('test.h5')
+os.remove("test.h5")
