0ff70d0f5c5ceb0d837c405524d8dcdfd330d15c
Alex Guinness <aguinnes@syr.edu>
2024-08-14 12:53:16 -0400

    edit existing example

--- examples/indiv_funcs/hdf_test.py
+++ examples/indiv_funcs/hdf_test.py
@@ -7,8 +7,10 @@ a = nddata(r_[0:9],[3,3],['a','b']).labels(
             b = r_[1:4],
             ))
 a.name('test_data')
+a.set_units('a','s')
 a.hdf5_write('test.h5')
 a_reload = nddata_hdf5('test.h5/test_data')
+print("Units set for the a axis are:", a_reload.get_units('a'))
 assert all(a.data == a_reload.data)
 assert all(a.getaxis('a') == a_reload.getaxis('a'))
 assert all(a.getaxis('b') == a_reload.getaxis('b'))
