3678d95cd21cbb54501da3eabe65e239911d4598
Alex Guinness <aguinnes@syr.edu>
2024-11-11 15:07:12 -0500

    edit to pass flake

--- examples_undoc/unit_checking.py
+++ examples_undoc/unit_checking.py
@@ -1,9 +1,9 @@
-from pyspecdata import *
+from pyspecdata import nddata, r_
 
 a = nddata(r_[0:1e-6:500j], "t").set_units("t", "s")
 a.human_units()
 print("this should be 1e-9", a.div_units("t", "s"))
 try:
     print(a.div_units("t", "T"))
-except:
+except Exception:
     print("yay! I failed when I should have")
