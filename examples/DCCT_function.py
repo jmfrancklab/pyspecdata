8c89d934997cc848064a2654b7139fb3cadd85ce
Alex Guinness <aguinnes@syr.edu>
2024-11-18 10:17:28 -0500

    debugging envt

--- examples/DCCT_function.py
+++ examples/DCCT_function.py
@@ -11,24 +11,27 @@ domain as well as visualizing the inversion of phase
 using the domain colored plotting style.
 """
 
-from pylab import rcParams
+from pylab import *
+from pyspecdata import *
 import pyspecdata as psd
-from pyspecdata import r_
-from numpy.random import seed
-import matplotlib.pyplot as plt
+from pyspecdata.DCCT_function import DCCT
+from numpy.random import normal, seed
 import sympy as s
 from collections import OrderedDict
+import sys
 
 seed(2021)
+print(sys.path)
+print(psd.__file__)
 rcParams["image.aspect"] = "auto"  # needed for sphinx gallery
 # sphinx_gallery_thumbnail_number = 2
-psd.init_logging(level="debug")
+init_logging(level="debug")
 
-with psd.figlist_var() as fl:
+with figlist_var() as fl:
     # provide the symbols that we use for the fake data:
     t2, td, vd, ph1, ph2 = s.symbols("t2 td vd ph1 ph2")
     echo_time = 5e-3
-    data = psd.fake_data(
+    data = fake_data(
         # Give the functional form of the fake data.
         # This is an inversion recovery with
         # T₁ of 0.2
@@ -45,10 +48,10 @@ with psd.figlist_var() as fl:
         # time-dependent resonance variation -- see fake_data doc.)
         OrderedDict(
             [
-                ("vd", psd.nddata(r_[0:1:40j], "vd")),
-                ("ph1", psd.nddata(r_[0, 2] / 4.0, "ph1")),
-                ("ph2", psd.nddata(r_[0:4] / 4.0, "ph2")),
-                ("t2", psd.nddata(r_[0:0.2:256j] - echo_time, "t2")),
+                ("vd", nddata(r_[0:1:40j], "vd")),
+                ("ph1", nddata(r_[0, 2] / 4.0, "ph1")),
+                ("ph2", nddata(r_[0:4] / 4.0, "ph2")),
+                ("t2", nddata(r_[0:0.2:256j] - echo_time, "t2")),
             ]
         ),
         {"ph1": 0, "ph2": 1},
@@ -65,92 +68,92 @@ with psd.figlist_var() as fl:
     #                   DCCT
     # keyword arguments to use for the first DCCT
     dcct_kwargs_1 = dict(
-        bbox=[LHS_pad_1, bbox_bottom, R_to_L], total_spacing=0.2, top_pad=0.1
+        DCCT_bbox=[LHS_pad_1, bbox_bottom, R_to_L], total_spacing=0.2, top_pad=0.1
     )
     fig = fl.next("Data")  # Make figure object to place the DCCT
     data.ft(["ph1", "ph2"])
-    psd.DCCT(data, fig, plot_title="Time Domain", **dcct_kwargs_1)
+    DCCT(data, fig, plot_title="Time Domain", **dcct_kwargs_1)
     data.ft("t2")
     # keyword arguments to use for second DCCT
     dcct_kwargs_2 = dict(
-        bbox=[LHS_pad_2, bbox_bottom, R_to_L], total_spacing=0.2, top_pad=0.1
+        DCCT_bbox=[LHS_pad_2, bbox_bottom, R_to_L], total_spacing=0.2, top_pad=0.1
     )
-    psd.DCCT(data, fig, plot_title="Frequency Domain", **dcct_kwargs_2)
+    DCCT(data, fig, plot_title="Frequency Domain", **dcct_kwargs_2)
     # {{{ add lines indicating kwargs
-    plt.plot(
-        [0.06, 0.06],
-        [0.0, 0.09],
-        "b",
-        marker="_",
-        linewidth=1,
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.text(
-        0.01,
-        0.03,
-        "bbox[1]",
-        color="b",
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.plot(
-        [0, 0.1],
-        [0.9, 0.9],
-        "b",
-        marker="|",
-        linewidth=1,
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.text(
-        0.025,
-        0.92,
-        "bbox[0]",
-        color="b",
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.plot(
-        [0.1, 0.52],
-        [0.95, 0.95],
-        "b",
-        marker="|",
-        linewidth=1,
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.text(
-        0.35,
-        0.97,
-        "bbox[2]",
-        color="b",
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.text(
-        0.35,
-        0.97,
-        "bbox[2]",
-        color="b",
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.plot(
-        [0.12, 0.15],
-        [0.47, 0.47],
-        "r",
-        marker="|",
-        linewidth=1,
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    plt.text(
-        0.06,
-        0.49,
-        "kwarg(vert_label_space)",
-        color="r",
-        clip_on=False,
-        transform=fig.transFigure,
-    )
-    # }}}
+    #plt.plot(
+    #    [0.06, 0.06],
+    #    [0.0, 0.09],
+    #    "b",
+    #    marker="_",
+    #    linewidth=1,
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.text(
+    #    0.01,
+    #    0.03,
+    #    "bbox[1]",
+    #    color="b",
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.plot(
+    #    [0, 0.1],
+    #    [0.9, 0.9],
+    #    "b",
+    #    marker="|",
+    #    linewidth=1,
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.text(
+    #    0.025,
+    #    0.92,
+    #    "bbox[0]",
+    #    color="b",
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.plot(
+    #    [0.1, 0.52],
+    #    [0.95, 0.95],
+    #    "b",
+    #    marker="|",
+    #    linewidth=1,
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.text(
+    #    0.35,
+    #    0.97,
+    #    "bbox[2]",
+    #    color="b",
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.text(
+    #    0.35,
+    #    0.97,
+    #    "bbox[2]",
+    #    color="b",
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.plot(
+    #    [0.12, 0.15],
+    #    [0.47, 0.47],
+    #    "r",
+    #    marker="|",
+    #    linewidth=1,
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    #plt.text(
+    #    0.06,
+    #    0.49,
+    #    "kwarg(vert_label_space)",
+    #    color="r",
+    #    clip_on=False,
+    #    transform=fig.transFigure,
+    #)
+    ## }}}
