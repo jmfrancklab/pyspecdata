7a2a862629c929b0e1eee631b326d22f88edbb7a
John Franck <jmfranck@jmfDebian.syr.edu>
2024-12-23 18:44:05 -0500

    make flake happy with nnls

--- pyspecdata/nnls.py
+++ pyspecdata/nnls.py
@@ -1,19 +1,20 @@
 # again, this is copied liberally from scipy nnls -- see scipy licensing
 
-from .general_functions import redim_F_to_C, redim_C_to_F, strm, inside_sphinx
+from .general_functions import redim_F_to_C, redim_C_to_F, strm
 import _nnls
 import numpy as np
 from numpy import asarray_chkfinite, zeros, double, isscalar, isfortran
 import multiprocessing.dummy as mpd
 from multiprocessing import cpu_count
 import logging
-logger = logging.getLogger('pyspecdata.nnls')
 
-__all__ = ['nnls_regularized']
+logger = logging.getLogger("pyspecdata.nnls")
+
+__all__ = ["nnls_regularized"]
 
 
 def nnls_regularized(A, b, l=0, maxiter=None):
-    """
+    r"""
     Solve math:`argmin_x || Ax - b ||_2^2 + \lambda^2 ||x||_2^2` for ``x>=0``.
     This is a wrapper for a FORTRAN non-negative least squares solver,
     with regularization (added by stacking $A$ on top an identity matrix
@@ -54,19 +55,30 @@ def nnls_regularized(A, b, l=0, maxiter=None):
     Lawson C., Hanson R.J., (1987) Solving Least Squares Problems, SIAM
 
     """
-    logger.debug(strm("isfortran result",isfortran(A),isfortran(b)))
+    logger.debug(strm("isfortran result", isfortran(A), isfortran(b)))
 
     A, b = list(map(asarray_chkfinite, (A, b)))
 
     if len(A.shape) != 2:
         raise ValueError("expected matrix")
     if len(b.shape) > 2:
-        raise ValueError("Expected vector! It's allowed to have indirect, but you gave me, "+str(b.shape))
+        raise ValueError(
+            "Expected vector! It's allowed to have indirect, but you gave me, "
+            + str(b.shape)
+        )
 
     m, n = A.shape
 
     if m != b.shape[-1]:
-        raise ValueError(strm("incompatible dimensions (rows of A",m," do not match size of data",b.shape,")"))
+        raise ValueError(
+            strm(
+                "incompatible dimensions (rows of A",
+                m,
+                " do not match size of data",
+                b.shape,
+                ")",
+            )
+        )
 
     maxiter = -1 if maxiter is None else int(maxiter)
 
@@ -78,34 +90,44 @@ def nnls_regularized(A, b, l=0, maxiter=None):
             x, rnorm, mode = _nnls.nnls(A, b, w, zz, index, maxiter)
         else:
             w = zeros((n,), dtype=double)
-            zz = zeros((m+n,), dtype=double)
+            zz = zeros((m + n,), dtype=double)
             index = zeros((n,), dtype=int)
             # choose the correct subroutine based on the dimension
             if len(b.shape) == 1:
-                x, rnorm, mode = _nnls.nnls_regularized(A, b, w, zz, index, maxiter, l)
+                x, rnorm, mode = _nnls.nnls_regularized(
+                    A, b, w, zz, index, maxiter, l
+                )
             if len(b.shape) == 2:
-                x, rnorm, mode = _nnls.nnls_regularized_loop(A, redim_C_to_F(b), w, zz, index, maxiter, l)
+                x, rnorm, mode = _nnls.nnls_regularized_loop(
+                    A, redim_C_to_F(b), w, zz, index, maxiter, l
+                )
                 x = redim_F_to_C(x)
     else:
-            nCPU = cpu_count() 
-            #print("I found",nCPU,"CPU's")
-            p = mpd.Pool(nCPU)
-            if len(b.shape) == 1:
-                def nnls_func(l):
-                    w = zeros((n,), dtype=double)
-                    zz = zeros((m+n,), dtype=double)
-                    index = zeros((n,), dtype=int)
-                    return _nnls.nnls_regularized(A, b, w, zz, index, maxiter, l)
-            if len(b.shape) == 2:
-                def nnls_func(l):
-                    w = zeros((n,), dtype=double)
-                    zz = zeros((m+n,), dtype=double)
-                    index = zeros((n,), dtype=int)
-                    x, rnorm, mode = _nnls.nnls_regularized_loop(A, redim_C_to_F(b), w, zz, index, maxiter, l)
-                    return redim_F_to_C(x), rnorm, mode
-            retval = p.map(nnls_func,l)
-            x,rnorm,mode = list(map(np.array,list(zip(*retval))))
-    if (isscalar(mode) and mode != 1):
+        nCPU = cpu_count()
+        # print("I found",nCPU,"CPU's")
+        p = mpd.Pool(nCPU)
+        if len(b.shape) == 1:
+
+            def nnls_func(l):
+                w = zeros((n,), dtype=double)
+                zz = zeros((m + n,), dtype=double)
+                index = zeros((n,), dtype=int)
+                return _nnls.nnls_regularized(A, b, w, zz, index, maxiter, l)
+
+        elif len(b.shape) == 2:
+
+            def nnls_func(l):
+                w = zeros((n,), dtype=double)
+                zz = zeros((m + n,), dtype=double)
+                index = zeros((n,), dtype=int)
+                x, rnorm, mode = _nnls.nnls_regularized_loop(
+                    A, redim_C_to_F(b), w, zz, index, maxiter, l
+                )
+                return redim_F_to_C(x), rnorm, mode
+
+        retval = p.map(nnls_func, l)
+        x, rnorm, mode = list(map(np.array, list(zip(*retval))))
+    if isscalar(mode) and mode != 1:
         # need something for the multiple lambda
         raise RuntimeError("too many iterations")
     return x, rnorm
