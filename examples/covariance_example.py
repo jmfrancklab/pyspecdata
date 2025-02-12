29e7e794fc30ab4b50424b50ffc2cdda79410584
Alex Guinness <aguinnes@syr.edu>
2025-02-12 13:23:26 -0500

    AG clean up

--- examples/covariance_example.py
+++ examples/covariance_example.py
@@ -19,16 +19,19 @@ with psd.figlist_var() as fl:
     # I explictly reorder so I know which dimension is which when I do raw
     # numpy and matplotlib
     thisdata.reorder(["t2", "nScans"])
-    mycov = abs(np.cov(thisdata.data))
+    # {{{ Covariance-variance matrix using numpy
+    npcov = abs(np.cov(thisdata.data))
     fl.next("covariance -- numpy")
-    fl.image(mycov)
-    # Make copy since the data is returned with the subscript axes
-    pycov = abs(thisdata.C.cov_mat("nScans"))
-    fl.next("covariance - pyspecdata")
-    fl.image(pycov)
+    fl.image(npcov)
+    # }}}
+    # {{{ Covariance-variance matrix using pyspecdata
+    psdcov = abs(thisdata.cov_mat("nScans"))
+    fl.next("covariance -- pyspecdata")
+    fl.image(psdcov)
     # now I zoom in to show that when I'm in the part of the FID that's
     # just noise (no shifting frequencies, I get the diagonal covariance
     # that I expect)
-    zoom_pycov = abs(thisdata["t2":(0.6, 0.7)].cov_mat("nScans"))
-    fl.next("covariance - FID tail")
+    zoom_pycov = abs(thisdata["$t2_i$":(0.6, 0.7)]["$t2_j$":(0.6, 0.7)])
+    fl.next("FID tail covariance -- pyspecdata")
     fl.image(zoom_pycov)
+    # }}}
