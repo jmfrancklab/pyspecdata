4a9f4d2e7aca8fbf43f22dfc7a725caa8bcb7211
jmfranck <jmfranck@users.noreply.github.com>
2025-06-06 22:58:12 -0400

    Improve conftest dependency stubbing

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,67 @@ def load_module(name: str):
     sys.modules.setdefault("_nnls", types.ModuleType("_nnls"))
     sys.modules.setdefault("tables", types.ModuleType("tables"))
     sys.modules.setdefault("h5py", types.ModuleType("h5py"))
+    try:
+        import matplotlib  # noqa: F401
+    except Exception:
+        mpl_stub = types.ModuleType("matplotlib")
+        pylab_stub = types.ModuleType("matplotlib.pylab")
+
+        class DummyImg:
+            def get_clim(self):
+                return (-1, 1)
+
+            def set_clim(self, *_args):
+                pass
+
+        def gci():
+            return DummyImg()
+
+        pylab_stub.gci = gci
+        pylab_stub.pi = np.pi
+        pylab_stub.r_ = np.r_
+        mpl_stub.pylab = pylab_stub
+        sys.modules["matplotlib"] = mpl_stub
+        sys.modules["matplotlib.pylab"] = pylab_stub
+        sys.modules.setdefault("pylab", pylab_stub)
+    try:
+        import mpl_toolkits.mplot3d  # noqa: F401
+    except Exception:
+        mpl_toolkits_stub = types.ModuleType("mpl_toolkits")
+        mplot3d_stub = types.ModuleType("mpl_toolkits.mplot3d")
+        mplot3d_stub.axes3d = types.SimpleNamespace()
+        mpl_toolkits_stub.mplot3d = mplot3d_stub
+        sys.modules["mpl_toolkits"] = mpl_toolkits_stub
+        sys.modules["mpl_toolkits.mplot3d"] = mplot3d_stub
+    try:
+        import pint  # noqa: F401
+    except Exception:
+        pint_stub = types.ModuleType("pint")
+
+        class DummyQuantity:
+            def __init__(self, magnitude, units=""):
+                self.magnitude = magnitude
+                self.units = units
+
+            def to_base_units(self):
+                return self
+
+        class DummyUnitRegistry:
+            Quantity = DummyQuantity
+
+            def __call__(self, *args):
+                if len(args) == 1:
+                    mag = 1
+                    units = args[0]
+                else:
+                    mag, units = args
+                return DummyQuantity(mag, units)
+
+            def define(self, *_args, **_kwargs):
+                pass
+
+        pint_stub.UnitRegistry = DummyUnitRegistry
+        sys.modules["pint"] = pint_stub
     if "numpy.core.rec" not in sys.modules:
         rec = types.ModuleType("rec")
         rec.fromarrays = np.core.records.fromarrays
