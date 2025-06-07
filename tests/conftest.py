3b536c5dabf05b9cae54861d2f93851adc56afdf
jmfranck <jmfranck@users.noreply.github.com>
2025-06-06 23:48:40 -0400

    Stub matplotlib, sympy, and scipy for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,6 +19,8 @@ def load_module(name: str):
     except Exception:
         mpl_stub = types.ModuleType("matplotlib")
         pylab_stub = types.ModuleType("matplotlib.pylab")
+        ticker_stub = types.ModuleType("matplotlib.ticker")
+        transforms_stub = types.ModuleType("matplotlib.transforms")
 
         class DummyImg:
             def get_clim(self):
@@ -30,12 +32,85 @@ def load_module(name: str):
         def gci():
             return DummyImg()
 
+        class DummyAx:
+            def __init__(self):
+                pass
+
+        def gca():
+            return DummyAx()
+
+        def sca(_ax):
+            pass
+
+        def imshow(*_args, **_kwargs):
+            return DummyImg()
+
+        def plot(*_args, **_kwargs):
+            return DummyImg()
+
+        def xlabel(*_args, **_kwargs):
+            pass
+
+        def ylabel(*_args, **_kwargs):
+            pass
+
+        def title(*_args, **_kwargs):
+            pass
+
+        def colorbar(*_args, **_kwargs):
+            return DummyImg()
+
+        def setp(*_args, **_kwargs):
+            pass
+
+        def rc(*_args, **_kwargs):
+            pass
+
         pylab_stub.gci = gci
+        pylab_stub.gca = gca
+        pylab_stub.sca = sca
+        pylab_stub.imshow = imshow
+        pylab_stub.plot = plot
+        pylab_stub.xlabel = xlabel
+        pylab_stub.ylabel = ylabel
+        pylab_stub.title = title
+        pylab_stub.colorbar = colorbar
+        pylab_stub.setp = setp
+        pylab_stub.cm = types.SimpleNamespace(gray=None)
+        pylab_stub.rc = rc
+        pylab_stub.rcParams = {
+            "axes.prop_cycle": types.SimpleNamespace(by_key=lambda: {"color": ["k"]})
+        }
         pylab_stub.pi = np.pi
         pylab_stub.r_ = np.r_
+        ticker_stub.AutoMinorLocator = type("AutoMinorLocator", (), {})
+        ticker_stub.MaxNLocator = type("MaxNLocator", (), {"__init__": lambda self, *a, **k: None})
+        class DummyBbox:
+            @staticmethod
+            def union(_bboxes):
+                return DummyBbox()
+
+            def transformed(self, *_args):
+                return self
+
+            @property
+            def width(self):
+                return 0
+
+            @property
+            def height(self):
+                return 0
+
+        transforms_stub.Bbox = DummyBbox
         mpl_stub.pylab = pylab_stub
+        mpl_stub.pyplot = pylab_stub
+        mpl_stub.ticker = ticker_stub
+        mpl_stub.transforms = transforms_stub
         sys.modules["matplotlib"] = mpl_stub
         sys.modules["matplotlib.pylab"] = pylab_stub
+        sys.modules["matplotlib.pyplot"] = pylab_stub
+        sys.modules["matplotlib.ticker"] = ticker_stub
+        sys.modules["matplotlib.transforms"] = transforms_stub
         sys.modules.setdefault("pylab", pylab_stub)
     try:
         import mpl_toolkits.mplot3d  # noqa: F401
@@ -116,6 +191,49 @@ def load_module(name: str):
 
         pint_stub.UnitRegistry = DummyUnitRegistry
         sys.modules["pint"] = pint_stub
+    try:
+        import sympy  # noqa: F401
+    except Exception:
+        sympy_stub = types.ModuleType("sympy")
+
+        class Expr:
+            pass
+
+        class Symbol(Expr):
+            def __init__(self, name):
+                self.name = name
+
+        def sqrt(x):
+            return np.sqrt(x)
+
+        def nsimplify(x, *args, **kwargs):
+            return x
+
+        sympy_stub.Expr = Expr
+        sympy_stub.Symbol = Symbol
+        sympy_stub.sqrt = sqrt
+        sympy_stub.nsimplify = nsimplify
+        sympy_stub.I = 1j
+        sympy_stub.pi = np.pi
+        sympy_stub.utilities = types.SimpleNamespace(lambdify=lambda *a, **k: None)
+        fem = types.ModuleType("sympy.functions.elementary.miscellaneous")
+        fem.sqrt = sqrt
+        sys.modules["sympy"] = sympy_stub
+        sys.modules["sympy.functions"] = types.ModuleType("sympy.functions")
+        sys.modules["sympy.functions.elementary"] = types.ModuleType("sympy.functions.elementary")
+        sys.modules["sympy.functions.elementary.miscellaneous"] = fem
+    try:
+        import scipy  # noqa: F401
+    except Exception:
+        scipy_stub = types.ModuleType("scipy")
+        sparse_stub = types.ModuleType("scipy.sparse")
+        interpolate_stub = types.ModuleType("scipy.interpolate")
+        interpolate_stub.interp1d = lambda *a, **k: None
+        scipy_stub.sparse = sparse_stub
+        scipy_stub.interpolate = interpolate_stub
+        sys.modules["scipy"] = scipy_stub
+        sys.modules["scipy.sparse"] = sparse_stub
+        sys.modules["scipy.interpolate"] = interpolate_stub
     if "numpy.core.rec" not in sys.modules:
         rec = types.ModuleType("rec")
         rec.fromarrays = np.core.records.fromarrays
