7fe942711eee8e11766bdd09062f7cb68929c880
John Franck <jmfranck@jmfDebian.syr.edu>
2025-07-08 11:34:35 -0400

    this one works

---
+++ examples/2DILT.py
@@ -0,0 +1,62 @@
+from pylab import *
+from pyspecdata import *
+
+# Here, we're going to provide a few demonstrations of the ILT functionality.  Let's start with Fig 1.10 in A. Beaton's thesis, which is based off the figures
+# in Venkataramanan.
+# 
+# pySpecData makes it easy to construct fake data like this: let's see how!
+
+NT1 = 50 # Number of T1 values
+NT2 = 50 # Number of T2 values
+LT1_name = r'$\log(T_1)$'
+LT1 = nddata(linspace(-2.5,0.5,NT1), LT1_name)
+LT2_name = r'$\log(T_2)$'
+LT2 = nddata(linspace(-2.5,0.3,NT2), LT2_name)
+mu = [-1.25,-1.75]
+sigma = [0.1,0.1]
+exact_data = exp(-(LT1-mu[0])**2/2/sigma[0]**2-(LT2-mu[1])**2/2/sigma[1]**2)
+slanted_coord1 = (LT1+LT2)/sqrt(2)
+slanted_coord2 = (LT2-LT1)/sqrt(2)
+mu = [-1.0,-0.4]
+mu = [ # convert to slanted coords
+        (mu[0]+mu[1])/sqrt(2),
+        (mu[1]-mu[0])/sqrt(2)]
+sigma = [0.5,0.05] # in slanted
+exact_data += exp(-(slanted_coord1-mu[0])**2/2/sigma[0]**2-(slanted_coord2-mu[1])**2/2/sigma[1]**2)
+exact_data.reorder(LT2_name) # T₂ along y axis
+
+figure(1)
+title("exact data")
+image(exact_data)
+
+# Now add the experimental decay dimensions
+
+tau1 = nddata(logspace(log10(5.0e-4),log10(4),30),'tau1')
+tau2 = nddata(linspace(5.0e-4,3.8,1000),'tau2')
+
+# $$T_1 = 10^{\log(T_1)}$$
+# $$R_1 = 10^{-\log(T_1)}$$
+# $$\ln(R_1) = -\log(T_1) \ln(10)$$
+
+basis = exp(-tau2/10**LT2)*(1-2*exp(-tau1/10**LT1))
+
+# Sum along the distribution dimensions, to create fake data
+# then add noise, and scale data so that noise has norm of 1
+
+simulated_data = basis*exact_data
+simulated_data.sum(LT1_name).sum(LT2_name)
+simulated_data.add_noise(0.1)
+simulated_data /= 0.1
+
+
+# use BRD to find the value of $\lambda$ 
+
+simulated_data.nnls(('tau1','tau2'),(LT1,LT2),
+                    (lambda tau1,LT1: 1-2*exp(-tau1*10**-LT1),
+                     lambda tau2,LT2: exp(-tau2*10**-LT2)), l='BRD')
+
+figure(2)
+title("BRD")
+image(simulated_data)
+
+show()
--- nnls/venk_brd.f90
+++ nnls/venk_brd.f90
@@ -1,13 +1,13 @@
-      subroutine venk_brd(initial_alpha,k0,m,n,mr,f,alpha_out,tol,maxiter)
+      subroutine venk_brd(initial_alpha,k0_mat,m,n,m_r,f,alpha_out,tol,maxiter)
 !f2py threadsafe
 ! implement the Butler–Reeds–Dawson solver in Fortran
       integer,intent(in) :: m,n,maxiter
       double precision,intent(in) :: initial_alpha,tol
-      double precision,intent(in) :: k0(m,n),mr(m)
+      double precision,intent(in) :: k0_mat(m,n),m_r(m)
       double precision,intent(out) :: f(n),alpha_out
       double precision,allocatable :: c(:),c_new(:),grad(:),newgrad(:)
-      double precision,allocatable :: delta(:)
-      double precision,allocatable :: G(:,:),H(:,:),H_copy(:,:),hd(:,:),K0t(:)
+      double precision,allocatable :: delta_c(:)
+      double precision,allocatable :: g_mat(:,:),h_mat(:,:),h_mat_copy(:,:),k0_t(:)
       double precision,allocatable :: tempvec(:)
       integer,allocatable :: piv(:)
       double precision :: alpha,alpha_new,sqrt_n
@@ -16,51 +16,54 @@
       external dgesv
       double precision :: norm_grad,norm_mr
       sqrt_n = sqrt(dble(m))
-      alpha = 1.0d-3
-      allocate(c(m),c_new(m),grad(m),newgrad(m),delta(m),G(m,m),H(m,m),H_copy(m,m),hd(m,1),K0t(n),tempvec(m))
+      alpha = initial_alpha
+      allocate(c(m),c_new(m),grad(m),newgrad(m),delta_c(m),g_mat(m,m),h_mat(m,m),h_mat_copy(m,m),k0_t(n),tempvec(m))
       allocate(piv(m))
       c = 1.0d0
-      norm_mr = sqrt(sum(mr*mr))
+      norm_mr = norm2(m_r)
       do iter=1,maxiter
-         do j=1,100
-            ! IN: k0,m,n,c OUT: G,K0t
-            call compute_g(k0,m,n,c,G,K0t)
-            ! IN: G,c,m,alpha,mr OUT: grad
-            call compute_grad(G,c,m,alpha,mr,grad)
-            ! IN: G,m,alpha OUT: H
-            call add_diag(G,m,alpha,H)
-            delta = grad
-            H_copy = H
-            ! IN: m, 1, m, m INOUT: H,hd(:,1) OUT: piv,info
-            call dgesv(m,1,H_copy,m,piv,delta,m,info)
-            tempvec = matmul(H,delta)
-            denom = dot_product(delta,tempvec)
+        do j=1,100
+            ! IN: k0_mat,m,n,c OUT: g_mat,k0_t
+            call compute_g(k0_mat,m,n,c,g_mat,k0_t)
+            ! IN: g_mat,c,m,alpha,m_r OUT: grad
+            call compute_grad(g_mat,c,m,alpha,m_r,grad)
+            ! IN: g_mat,m,alpha OUT: h_mat
+            call add_diag(g_mat,m,alpha,h_mat)
+            delta_c = grad
+            h_mat_copy = h_mat
+            ! IN: m, 1, m, m INOUT: h_mat,delta_c OUT: piv,info
+            call dgesv(m,1,h_mat_copy,m,piv,delta_c,m,info)
+            tempvec = matmul(h_mat,delta_c)
+            denom = dot_product(delta_c,tempvec)
             if (denom == 0.d0) denom = 1.d-12
-            s = dot_product(delta,grad) / denom
-            c_new = c - s*delta
-            ! IN: G,c_new,m,alpha,mr OUT: newgrad
-            call compute_grad(G,c_new,m,alpha,mr,newgrad)
-            chi_old = chi_func(c,G,alpha,mr)
-            chi_new = chi_func(c_new,G,alpha,mr)
+            s = dot_product(delta_c,grad) / denom
+            c_new = c - s*delta_c
+            ! IN: g_mat,c_new,m,alpha,m_r OUT: newgrad
+            call compute_grad(g_mat,c_new,m,alpha,m_r,newgrad)
+            ! IN: c,g_mat,alpha,m_r OUT: chi_old
+            call chi_func(c,g_mat,alpha,m_r,chi_old)
+            ! IN: c_new,g_mat,alpha,m_r OUT: chi_new
+            call chi_func(c_new,g_mat,alpha,m_r,chi_new)
             k=0
             do while (chi_new >= chi_old .and. k < 20)
                k = k + 1
-               c_new = c - s*(0.5d0**k)*delta
-               ! IN: G,c_new,m,alpha,mr OUT: newgrad
-               call compute_grad(G,c_new,m,alpha,mr,newgrad)
-               chi_new = chi_func(c_new,G,alpha,mr)
+               c_new = c - s*(0.5d0**k)*delta_c
+               ! IN: g_mat,c_new,m,alpha,m_r OUT: newgrad
+               call compute_grad(g_mat,c_new,m,alpha,m_r,newgrad)
+               ! IN: c_new,g_mat,alpha,m_r OUT: chi_new
+               call chi_func(c_new,g_mat,alpha,m_r,chi_new)
             end do
             c = c_new
             grad = newgrad
-            norm_grad = sqrt(sum(grad*grad))
+            norm_grad = norm2(grad)
             if (norm_grad/norm_mr < 1.d-8) exit
          end do
-         alpha_new = sqrt_n / sqrt(sum(c*c))
+         alpha_new = sqrt_n / norm2(c)
          write(*,*) 'alpha iteration', iter, 'value', alpha_new
          if (abs(alpha_new-alpha)/alpha < tol) exit
          alpha = alpha_new
       end do
-      tempvec = matmul(transpose(k0),c)
+      tempvec = matmul(transpose(k0_mat),c)
       do j=1,n
          if (tempvec(j) > 0.d0) then
             f(j) = tempvec(j)
@@ -68,84 +71,55 @@
             f(j) = 0.d0
          end if
       end do
-      deallocate(c,c_new,grad,newgrad,delta,G,H,hd,K0t,tempvec,piv)
+      deallocate(c,c_new,grad,newgrad,delta_c,g_mat,h_mat,h_mat_copy,k0_t,tempvec,piv)
       alpha_out = alpha_new
       return
       contains
-         subroutine compute_g(k0,m,n,c,G,K0t)
+         subroutine compute_g(k0_mat,m,n,c,g_mat,k0_t)
             integer,intent(in)::m,n
-            double precision,intent(in)::k0(m,n),c(m)
-            double precision,intent(out)::G(m,m),K0t(n)
+            double precision,intent(in)::k0_mat(m,n),c(m)
+            double precision,intent(out)::g_mat(m,m),k0_t(n)
             integer :: p
-            G = 0.d0
-            K0t = 0.d0
+            g_mat = 0.d0
+            k0_t = 0.d0
             do p=1,n
-               K0t(p) = dot_product(k0(:,p),c)
-               if (K0t(p) > 0.d0) then
-                  G = G + outer_prod(k0(:,p))
+               k0_t(p) = dot_product(k0_mat(:,p),c)
+               if (k0_t(p) > 0.d0) then
+                  ! IN: k0_mat(:,p) INOUT: g_mat
+                  call add_outer(k0_mat(:,p),g_mat)
                end if
             end do
          end subroutine compute_g
-         function outer_prod(col) result(M)
+         subroutine add_outer(col,a_mat)
             double precision,intent(in)::col(:)
-            double precision :: M(size(col),size(col))
+            double precision,intent(inout)::a_mat(:,:)
             integer :: i,j
             do i=1,size(col)
                do j=1,size(col)
-                  M(i,j) = col(i)*col(j)
+                  a_mat(i,j) = a_mat(i,j) + col(i)*col(j)
                end do
             end do
-         end function outer_prod
-         subroutine compute_grad(G,c,m,alpha,mr,grad)
+         end subroutine add_outer
+         subroutine compute_grad(g_mat,c,m,alpha,m_r,grad)
             integer,intent(in)::m
-            double precision,intent(in)::G(m,m),c(m),alpha,mr(m)
+            double precision,intent(in)::g_mat(m,m),c(m),alpha,m_r(m)
             double precision,intent(out)::grad(m)
-            grad = matmul(G,c) + alpha*c - mr
+            grad = matmul(g_mat,c) + alpha*c - m_r
          end subroutine compute_grad
-        subroutine add_diag(G,m,alpha,H)
+        subroutine add_diag(g_mat,m,alpha,h_mat)
             integer,intent(in)::m
-            double precision,intent(in)::G(m,m),alpha
-            double precision,intent(out)::H(m,m)
+            double precision,intent(in)::g_mat(m,m),alpha
+            double precision,intent(out)::h_mat(m,m)
             integer::i
-            H = G
+            h_mat = g_mat
             do i=1,m
-               H(i,i) = H(i,i) + alpha
+               h_mat(i,i) = h_mat(i,i) + alpha
             end do
         end subroutine add_diag
 
-        subroutine solve_linear(a,b,x,n)
-            integer,intent(in) :: n
-            double precision,intent(in) :: a(n,n),b(n)
-            double precision,intent(out) :: x(n)
-            double precision :: aa(n,n), bb(n)
-            integer :: i,j,k
-            aa = a
-            bb = b
-            do k=1,n-1
-                do i=k+1,n
-                    if (aa(k,k) /= 0.d0) then
-                        bb(i) = bb(i) - aa(i,k)/aa(k,k)*bb(k)
-                        do j=k+1,n
-                            aa(i,j) = aa(i,j) - aa(i,k)/aa(k,k)*aa(k,j)
-                        end do
-                    end if
-                end do
-            end do
-            do i=n,1,-1
-                x(i) = bb(i)
-                do j=i+1,n
-                    x(i) = x(i) - aa(i,j)*x(j)
-                end do
-                if (aa(i,i) /= 0.d0) then
-                    x(i) = x(i)/aa(i,i)
-                else
-                    x(i) = 0.d0
-                end if
-            end do
-        end subroutine solve_linear
-         function chi_func(c,G,alpha,mr) result(val)
-            double precision,intent(in)::c(:),G(size(c),size(c)),alpha,mr(:)
-            double precision::val
-            val = dot_product(c,matmul(G,c)) + alpha*dot_product(c,c) - dot_product(c,mr)
-         end function chi_func
+         subroutine chi_func(c,g_mat,alpha,m_r,val)
+            double precision,intent(in)::c(:),g_mat(size(c),size(c)),alpha,m_r(:)
+            double precision,intent(out)::val
+            val = dot_product(c,matmul(g_mat,c)) + alpha*dot_product(c,c) - dot_product(c,m_r)
+         end subroutine chi_func
       end subroutine venk_brd
