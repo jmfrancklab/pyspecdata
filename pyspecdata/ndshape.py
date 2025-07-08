a4134266e90448b834b45e1bdd4c2ba8c453d64f
John Franck <jmfranck@jmfDebian.syr.edu>
2025-07-08 12:29:18 -0400

    flake happy

--- pyspecdata/ndshape.py
+++ pyspecdata/ndshape.py
@@ -1,10 +1,15 @@
-r'''The :class:`ndshape` class allows you to allocate arrays and determine the shape of existing arrays.'''
-import numpy as np
-from .general_functions import *
+r"""The :class:`ndshape` class allows you to allocate arrays and determine the
+shape of existing arrays."""
 
-class ndshape_base ():
-    r'''The base ndshape class, which doesn't include an allocation method.'''
-    def __init__(self,*args):
+import numpy as np
+from .general_functions import strm
+from copy import deepcopy
+
+
+class ndshape_base:
+    r"""The base ndshape class, which doesn't include an allocation method."""
+
+    def __init__(self, *args):
         """Create an nddata in one of various ways.  Values are
         always copies of the values they were created from
         (changing the shape of the initialization parameters will
@@ -14,19 +19,19 @@ class ndshape_base ():
 
         >>> nddata_instance = ndshape(list_of_pairs)
 
-        or 
+        or
 
         >>> nddata_instance = ndshape(nddata_instance)
 
         Parameters
         ==========
-        
+
         shapes : list of int
 
             the sizes of the dimensions, in order
 
         dimlabels : list of str
-            
+
             the names of the dimensions, in order
 
         list_of_pairs : list of tuples
@@ -34,7 +39,7 @@ class ndshape_base ():
             zip(dimlabels,shapes)
 
         nddata_instance : nddata
-            
+
             determine the shape of the nddata,
             and return it
         """
@@ -42,53 +47,95 @@ class ndshape_base ():
         if len(args) == 2:
             self.shape = list(args[0])
             self.dimlabels = args[1]
-        if len(args) == 1: #assum that it's an nddata object
-            if hasattr(args[0],'dimlabels') and hasattr(args[0],'data'):# test that it's nddata without using any of the functions in core.py
+        if len(args) == 1:  # assum that it's an nddata object
+            if hasattr(args[0], "dimlabels") and hasattr(
+                args[0], "data"
+            ):  # test that it's nddata without using any of the functions in
+                #     core.py
                 self.shape = list(args[0].data.shape)
                 self.dimlabels = list(args[0].dimlabels)
                 if len(self.shape) == 0 and len(self.dimlabels) == 0:
                     self.zero_dimensional = True
                     return
             elif isinstance(args[0], list):
-                self.dimlabels, self.shape = list(map(list,list(zip(*args[0]))))
+                self.dimlabels, self.shape = list(
+                    map(list, list(zip(*args[0])))
+                )
             else:
-                raise ValueError('If you pass a single argument, it must be an nddata')
+                raise ValueError(
+                    "If you pass a single argument, it must be an nddata"
+                )
         return
-    def __setitem__(self,reference,setto):
+
+    def __setitem__(self, reference, setto):
         self.shape[self.axn(reference)] = setto
         return
-    def axn(self,axis):
+
+    def axn(self, axis):
         r'return the number for the axis with the name "axis"'
         try:
             return self.dimlabels.index(axis)
-        except:
-            raise ValueError(' '.join(map(repr,['there is no axis named',axis,'all axes are named',self.dimlabels])))
+        except Exception:
+            raise ValueError(
+                " ".join(
+                    map(
+                        repr,
+                        [
+                            "there is no axis named",
+                            axis,
+                            "all axes are named",
+                            self.dimlabels,
+                        ],
+                    )
+                )
+            )
+
     def copy(self):
         try:
             return deepcopy(self)
-        except:
-            raise RuntimeError('Some type of error trying to run deepcopy on'+repr(self))
-    def matchdims(self,arg):
-        r'returns shape with [not in self, len 1] + [overlapping dims between arg + self] + [not in arg] --> this is better accomplished by using sets as I do in the matchdims below'
+        except Exception:
+            raise RuntimeError(
+                "Some type of error trying to run deepcopy on" + repr(self)
+            )
+
+    def matchdims(self, arg):
+        """returns shape with [not in self, len 1] + [overlapping dims
+        between arg + self] + [not in arg] --> this is better accomplished
+        by using sets as I do in the matchdims below"""
         for k in set(self.dimlabels) & set(arg.dimlabels):
             a = arg.shape[arg.axn(k)]
             b = self.shape[self.axn(k)]
             if a != b:
-                raise CustomError('the',k,'dimension is not the same for self',self,'and arg',arg)
-        if hasattr(arg,'dimlabels') and hasattr(arg,'data'):# test that it's nddata without using any of the functions in core.py
-            arg = ndshape(arg)
-        #{{{ add extra 1-len dims
-        addeddims = set(self.dimlabels) ^ set(arg.dimlabels) & set(arg.dimlabels)
+                raise RuntimeError(
+                    strm(
+                        "the",
+                        k,
+                        "dimension is not the same for self",
+                        self,
+                        "and arg",
+                        arg,
+                    )
+                )
+        if hasattr(arg, "dimlabels") and hasattr(
+            arg, "data"
+        ):  # test that it's nddata without using any of the functions in
+            # core.py
+            arg = arg.shape
+        # {{{ add extra 1-len dims
+        addeddims = set(self.dimlabels) ^ set(arg.dimlabels) & set(
+            arg.dimlabels
+        )
         self.dimlabels = list(addeddims) + self.dimlabels
         self.shape = [1] * len(addeddims) + list(self.shape)
-        #}}}
+        # }}}
         return self
-    def __radd__(self,arg):
-        'take list of shape,dimlabels'
+
+    def __radd__(self, arg):
+        "take list of shape,dimlabels"
         shape = arg[0]
         dimlabels = arg[1]
         if isinstance(shape, str):
-            shape,dimlabels = dimlabels,shape
+            shape, dimlabels = dimlabels, shape
         if np.isscalar(self.shape):
             self.shape = [self.shape]
         if np.isscalar(self.dimlabels):
@@ -100,13 +147,16 @@ class ndshape_base ():
         self.shape = shape + self.shape
         self.dimlabels = dimlabels + self.dimlabels
         return self
-    def __add__(self,arg):
-        '''take list of shape,dimlabels
-        this is the correct function, until I can fix my back-references for add, which does it backwards'''
+
+    def __add__(self, arg):
+        """take list of shape,dimlabels
+        this is the correct function, until I can fix my back-references for
+        add, which does it backwards
+        """
         shape = arg[0]
         dimlabels = arg[1]
         if isinstance(shape, str):
-            shape,dimlabels = dimlabels,shape
+            shape, dimlabels = dimlabels, shape
         if np.isscalar(self.shape):
             self.shape = [self.shape]
         if np.isscalar(self.dimlabels):
@@ -118,6 +168,7 @@ class ndshape_base ():
         self.shape = self.shape + shape
         self.dimlabels = self.dimlabels + dimlabels
         return self
+
     def __or__(self, other):
         r"""Combine two :class:`ndshape_base` objects.
 
@@ -152,41 +203,63 @@ class ndshape_base ():
         return ndshape_base(new_shape, new_dimlabels)
 
     __ror__ = __or__
-    def __repr__(self): #how it responds to print
-        return list(zip(self.shape,self.dimlabels)).__repr__()
+
+    def __repr__(self):  # how it responds to print
+        return list(zip(self.shape, self.dimlabels)).__repr__()
+
     def __iter__(self):
         self._index = 0
         return self
+
     def max(self):
         idx = np.argmax(self.shape)
         return self.dimlabels[idx]
+
     def min(self):
         idx = np.argmin(self.shape)
         return self.dimlabels[idx]
+
     def __next__(self):
         if self._index < len(self.shape):
-            k,v = self.dimlabels[self._index],self.shape[self._index]
+            k, v = self.dimlabels[self._index], self.shape[self._index]
             self._index += 1
-            return k,v
+            return k, v
         else:
             raise StopIteration
-    def __getitem__(self,args):
+
+    def __getitem__(self, args):
         try:
-            mydict = dict(list(zip(self.dimlabels,self.shape)))
-        except Exception as e:
-            raise ValueError(strm("either dimlabels=",self.dimlabels,"or shape",
-                self.shape,"not in the correct format") + explain_error(e))
+            mydict = dict(list(zip(self.dimlabels, self.shape)))
+        except Exception:
+            raise ValueError(
+                strm(
+                    "either dimlabels=",
+                    self.dimlabels,
+                    "or shape",
+                    self.shape,
+                    "not in the correct format",
+                )
+            )
         try:
             return mydict[args]
-        except:
-            raise ValueError(strm("one or more of the dimensions named",args,"do not exist in",self.dimlabels))
-    def rename(self,before,after):
-        r'rename a dimension'
+        except Exception:
+            raise ValueError(
+                strm(
+                    "one or more of the dimensions named",
+                    args,
+                    "do not exist in",
+                    self.dimlabels,
+                )
+            )
+
+    def rename(self, before, after):
+        r"rename a dimension"
         thisindex = self.axn(before)
         self.dimlabels[thisindex] = after
         return self
-    def pop(self,label):
-        r'remove a dimension'
+
+    def pop(self, label):
+        r"remove a dimension"
         thisindex = self.axn(label)
         self.dimlabels.pop(thisindex)
         self.shape.pop(thisindex)
