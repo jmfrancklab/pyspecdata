6b34acc2547c12b87733b8a1907c65c431c1b892
John Franck <jmfranck@jmfDebian.syr.edu>
2025-08-10 17:16:44 -0400

    flake happy

--- tests/test_hdf5_layout.py
+++ tests/test_hdf5_layout.py
@@ -29,9 +29,9 @@ class DummyGroup(dict):
             if type(v) is dict:
                 self[k] = DummyGroup(v)
             elif isinstance(v, str):
-                self[k] = v.encode('utf-8')
+                self[k] = v.encode("utf-8")
             elif isinstance(v, list) and isinstance(v[0], str):
-                self[k] = [v.encode('utf-8') for v in self[k]]
+                self[k] = [v.encode("utf-8") for v in self[k]]
 
 
 def _generate_nddata():
@@ -48,6 +48,7 @@ def _generate_nddata():
     a.other_info.update({"level1": {"level2": 5, "level2list": [1, 2, 3]}})
     return a
 
+
 def _generate_nddata_noerr():
     data = np.arange(6).reshape(2, 3) + 1j * np.arange(1, 7).reshape(2, 3)
     a = nddata(data, ["t", "f"])
@@ -59,15 +60,19 @@ def _generate_nddata_noerr():
     a.other_info.update({"level1": {"level2": 5, "level2list": [1, 2, 3]}})
     return a
 
+
 def _check_layout(g, a, haserr=True):
     assert "data" in g
-    dimlabels = [j.decode('utf-8') for j in g.attrs["dimlabels"]]
+    dimlabels = [j.decode("utf-8") for j in g.attrs["dimlabels"]]
     assert dimlabels[0] == "t"
     assert dimlabels[1] == "f"
-    if not "data_units" in g["data"].attrs:
-        raise KeyError(f"dir(g['data']):{dir(g['data'])}\ng.attrs.keys():{g['data'].attrs.keys()}")
+    if "data_units" not in g["data"].attrs:
+        raise KeyError(
+            f"dir(g['data']):{dir(g['data'])}\n"
+            f"g.attrs.keys():{g['data'].attrs.keys()}"
+        )
     if a.get_units() is not None:
-        assert g["data"].attrs["data_units"].decode('utf-8') == a.get_units()
+        assert g["data"].attrs["data_units"].decode("utf-8") == a.get_units()
     shape = tuple(len(g["axes"][name]["data"]) for name in dimlabels)
     data_vals = np.array(g["data"]["data"]).reshape(shape)
     np.testing.assert_allclose(data_vals.real, a.data.real)
@@ -78,14 +83,19 @@ def _check_layout(g, a, haserr=True):
     for name in dimlabels:
         np.testing.assert_allclose(g["axes"][name]["data"], a.getaxis(name))
         if haserr:
-            np.testing.assert_allclose(g["axes"][name]["error"], a.get_error(name))
-        assert g["axes"][name].attrs["axis_coords_units"].decode('utf-8') == a.get_units(name)
+            np.testing.assert_allclose(
+                g["axes"][name]["error"], a.get_error(name)
+            )
+        assert g["axes"][name].attrs["axis_coords_units"].decode(
+            "utf-8"
+        ) == a.get_units(name)
     assert "level1" in g["other_info"]
     lvl1 = g["other_info"]["level1"]
     assert lvl1.attrs["level2"] == 5
     np.testing.assert_array_equal(
         lvl1.attrs["level2list"]["LISTELEMENTS"], [1, 2, 3]
     )
+
 
 def _check_loaded(b, a):
     assert list(b.dimlabels) == list(a.dimlabels)
@@ -109,7 +119,8 @@ def test_hdf5_layout(tmp_path):
     with h5py.File(tmp_path / "sample.h5", "r") as f:
         assert "test_nd" in f
         _check_layout(f["test_nd"], a)
-    os.remove(os.path.join(str(tmp_path),"sample.h5"))
+    os.remove(os.path.join(str(tmp_path), "sample.h5"))
+
 
 def test_hdf5_layout_noerr(tmp_path):
     a = _generate_nddata_noerr()
@@ -117,12 +128,14 @@ def test_hdf5_layout_noerr(tmp_path):
     with h5py.File(tmp_path / "sample.h5", "r") as f:
         assert "test_nd" in f
         _check_layout(f["test_nd"], a, haserr=False)
-    os.remove(os.path.join(str(tmp_path),"sample.h5"))
+    os.remove(os.path.join(str(tmp_path), "sample.h5"))
+
 
 def test_state_layout():
     a = _generate_nddata()
     g = DummyGroup(a.__getstate__())
     _check_layout(g, a)
+
 
 def test_state_layout_noerr():
     a = _generate_nddata_noerr()
@@ -135,14 +148,15 @@ def test_nddata_hdf5_roundtrip(tmp_path):
     a.hdf5_write("sample.h5", directory=str(tmp_path))
     b = nddata_hdf5("sample.h5/test_nd", directory=str(tmp_path))
     _check_loaded(b, a)
-    os.remove(os.path.join(str(tmp_path),"sample.h5"))
+    os.remove(os.path.join(str(tmp_path), "sample.h5"))
+
 
 def test_nddata_hdf5_roundtrip_noerr(tmp_path):
     a = _generate_nddata_noerr()
     a.hdf5_write("sample.h5", directory=str(tmp_path))
     b = nddata_hdf5("sample.h5/test_nd", directory=str(tmp_path))
     _check_loaded(b, a)
-    os.remove(os.path.join(str(tmp_path),"sample.h5"))
+    os.remove(os.path.join(str(tmp_path), "sample.h5"))
 
 
 def test_nddata_pickle_roundtrip(tmp_path):
@@ -165,6 +179,7 @@ def test_state_hdf_dict_roundtrip(tmp_path):
     with h5py.File(tmp_path / "state.h5", "r") as f:
         _check_layout(f["test_nd"], a)
     os.remove(tmp_path / "state.h5")
+
 
 def test_state_hdf_dict_roundtrip_noerr(tmp_path):
     a = _generate_nddata_noerr()
